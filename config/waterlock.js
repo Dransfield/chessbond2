a395508f70cce8bb2a491f0922446786814d5382
root <slenkar@gmail.com>
2016-11-24 21:42:11 +0800

    h

--- config/waterlock.js
+++ config/waterlock.js
@@ -40,14 +40,24 @@ baseUrl: 'http://www.chessbond.com',
         redirectUri: 'http://www.chessbond.com/auth/google_oauth2/'
     },
    	{
-		name: "waterlock-facebook-auth",
+		/*name: "waterlock-facebook-auth",
 		appId: '204930219956613',
 		appSecret:'6246902d2ef94f1dbd083fb4946c694b',
 		fieldMap: {
 			'Picture':'picture',
 			'Cover':'cover',
 			'gender': 'gender'
-		}
+		}*/
+		name: "waterlock-facebook-auth",
+		appId: '204758053307163',
+		appSecret:'efc1758be36f4bfc488ea18f5680cb60',
+		fieldMap: {
+			'Picture':'picture',
+			'Cover':'cover',
+			'gender': 'gender'
+		},
+		redirectUri: 'http://www.chessbond.com/auth/facebook_oauth2/'
+		
 	},
 	{
       name:'waterlock-local-auth-bcryptjs',
