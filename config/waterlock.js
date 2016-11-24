1450a46c8b3e1b493165ef83c9c7d7cbf27ea404
root <slenkar@gmail.com>
2016-11-24 20:55:31 +0800

    h

--- config/waterlock.js
+++ config/waterlock.js
@@ -15,7 +15,8 @@ module.exports.waterlock = {
   //
   // used by auth methods for callback URI's using oauth and for password
   // reset links.
-  baseUrl: 'http://localhost:1337',
+  //baseUrl: 'http://localhost:1337',
+baseUrl: 'http://www.chessbond.com',
 
   // Auth Method(s)
   //
