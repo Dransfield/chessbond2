046e8512167863959e1d741b713f758cadfbea1f
root <slenkar@gmail.com>
2016-11-30 17:06:07 +0800

    gh

--- api/controllers/PageController.js
+++ api/controllers/PageController.js
@@ -11,7 +11,7 @@ deleteopengame:function(req,res){
 	(function(err){console.log(err);});
 	
 	sails.sockets.broadcast('openchessgameroom','deleteopengameevent', {gameid:req.param('gameid')});
-	console.log('broadcast deleteopengameevent');
+	console.log('broadcast deleteopengameevent'+JSON.stringify({gameid:req.param('gameid')}));
 	}	,
 	newopengame:function(req,res){
 	console.log("all params of new game"+req.allParams());
@@ -176,7 +176,7 @@ deleteopengame:function(req,res){
     },
     chessgamemove:function(req,res){
 		sails.sockets.broadcast(req.param('GameID'), 'chessgamemove',{room:req.param('GameID')});
-	console.log('chessgamemove'+req.param('GameID'));
+
 	return res.ok();
 	},
     	chatmsg:function(req,res){
