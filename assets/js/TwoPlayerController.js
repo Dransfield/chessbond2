5c372d7ac581681c8d32dd87910741bca2cf5e99
root <slenkar@gmail.com>
2016-12-02 19:39:52 +0800

    gh

--- assets/js/TwoPlayerController.js
+++ assets/js/TwoPlayerController.js
@@ -435,7 +435,7 @@ $scope.changeFavicon=function (src) {
   console.log("height"+square.height());
   console.log("<img style='position:absolute;height:"+square.height()+"px;top:"+position.top+"px;left:"+position.left+"px' src='/images/circle.png'>");
   //square.append("<img style='position:relative;height:"+square.height()+"px;top:"+position.top+"px;left:"+position.left+"px' src='/images/circle.png'>");
-  square.append("<img style='height:"+square.height()+"px;' src='/images/circle.png'>");
+  square.append("<img style='position:absolute;height:"+square.height()+"px;' src='/images/circle.png'>");
   
   console.log('move'+JSON.stringify(move));
 updateStatus(game,gameRecord,move);
