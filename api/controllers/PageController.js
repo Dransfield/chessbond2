de812c72a97779d484884a88e21cc56038e63780
root <slenkar@gmail.com>
2016-12-05 16:59:52 +0800

    gh

--- api/controllers/PageController.js
+++ api/controllers/PageController.js
@@ -7,15 +7,6 @@
 
 module.exports = {
 	
-	function SetHomePageInterval(user)
-	{
-		
-				if (user.OnHomePage=='false')
-				{sails.sockets.broadcast('openchessgameroom','userleft',{id:req.session.user.id});}
-				else
-				{user.OnHomePage='false';}
-					
-	}
 	
 	deletegame:function(req,res){
 	Chessgame.destroy({id:req.param('gameid')}).exec
@@ -215,14 +206,23 @@ deleteopengame:function(req,res){
 		console.log("recieved heartbeat from:"+req.param('name'));
 		User.findOne(req.session.user.id, function foundUser(err, user) {
 			user.OnHomePage='true';
-			setTimeout(SetHomePageInterval(user)
-			,6000);
+			user.SetForRemovalFromHomePage='false';
+			setTimeout(function(){
+				
+		
+				if (user.SetForRemovalFromHomePage=='true')
+				{sails.sockets.broadcast('openchessgameroom','userleft',{id:req.session.user.id});}
+				else
+				{user.SetForRemovalFromHomePage='true';
+					
+					}
+					
+	}
+			,4000);
 		});
 	},
 	
-	UpdateHomePageChatRoom:function(req,res){
-	
-	},
+
     chessgamemove:function(req,res){
 		sails.sockets.broadcast(req.param('GameID'), 'chessgamemove',{room:req.param('GameID')});
 	setTimeout(		function(){sails.sockets.broadcast(req.param('GameID'), 'timeevent',{msg:"Two Seconds have passed"});}
