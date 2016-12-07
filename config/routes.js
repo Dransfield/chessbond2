f538c3bc0e52bbf7fbab0205e0c43a629a74c9ff
root <slenkar@gmail.com>
2016-12-07 16:58:26 +0800

    h

--- config/routes.js
+++ config/routes.js
@@ -34,7 +34,7 @@ module.exports.routes = {
 	'GET /': {view: 'homepage'},
 	'/profile': {view:'profile'},
 	'/playvsai':{view:'humanvsai'},
-	'GET /registerpage':{view:'registerpage'},
+	'GET /registerpage':{view:'register'},
 	
 	'GET /HomepageHeartbeat':'PageController.HomepageHeartbeat',
 	'/humanvshuman':{view:'humanvshuman'},
