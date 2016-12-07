d215735b1d454be5c9f83a50eec2e11b5f63fce3
root <slenkar@gmail.com>
2016-12-08 07:18:31 +0800

    h

--- assets/js/RegisterController.js
+++ assets/js/RegisterController.js
@@ -13,7 +13,7 @@ io.socket.post("/register",{name:$scope.vm.user.username,email: $scope.vm.user.e
 {
 	console.log(jwres);
 	console.log(resData);			
-			console.log(jwres.error.details);
+			console.log(JSON.stringify(jwres.error.details));
 			if(jwres.error)
 			{
 			toastr.info(jwres.error.details);
