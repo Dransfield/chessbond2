aade20ac0528f1d499944d705efc24308aa0cecf
root <slenkar@gmail.com>
2016-12-14 09:35:14 +0800

    h

--- assets/js/TwoPlayerController.js
+++ assets/js/TwoPlayerController.js
@@ -607,6 +607,7 @@ $scope.changeFavicon=function (src) {
 							  if (game.in_checkmate())
 								{
 							  toastr.success("Checkmate!");
+							  console.log("checkmate");
 							  $scope.RecordGameResult(me);
 							 }
 	 
@@ -632,8 +633,9 @@ $scope.changeFavicon=function (src) {
 					  //console.log("height"+square.height());
 					 // console.log("<img style='position:absolute;height:"+square.height()+"px;top:"+position.top+"px;left:"+position.left+"px' src='/images/circle.png'>");
 					  //square.append("<img style='position:relative;height:"+square.height()+"px;top:"+position.top+"px;left:"+position.left+"px' src='/images/circle.png'>");
-
+					
 					  console.log('move'+JSON.stringify(move));
+					console.log("result: "+$scope.ChessGameObject.Result);
 					updateStatus(game,$scope.ChessGameObject,move);
 };
 
