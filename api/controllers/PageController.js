ecd139ab00ed107c7df314d22ad62b0c16e9751a
root <slenkar@gmail.com>
2016-12-15 11:32:18 +0800

    h

--- api/controllers/PageController.js
+++ api/controllers/PageController.js
@@ -292,7 +292,7 @@ deleteopengame:function(req,res){
 	
 	var Res1=winnerRecord.name+"'s ELO score went from "+winnerstartELO+" to "+winnerRecord.ELO;
 	var Res2=loserRecord.name+"'s ELO score went from "+loserstartELO+" to "+loserRecord.ELO;
-	console.log("GameID "+req.param('GameID')):
+	console.log("GameID "+req.param('GameID'));
 	Chessgame.update({id:req.param('GameID')},{EloResult1:Res1,ELOResult2:Res2}).exec(function afterwards(err, updated){
 	sails.sockets.broadcast(req.param('GameID'), 'ELOAdjustments',updated);
 	console.log("ELO results"+JSON.stringify(updated));
