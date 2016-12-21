7d9b88a0fb047ba24748842c5e28f5e380fd77f2
root <slenkar@gmail.com>
2016-12-21 15:51:13 +0800

    h

--- assets/js/TwoPlayerController.js
+++ assets/js/TwoPlayerController.js
@@ -152,13 +152,13 @@ $scope.pic1height=200; $scope.pic1coordx=0;	$scope.pic1coordy=-60;
 }
 
 
-$scope.pic2height=25;
-$scope.pic2coordy=50;
-$scope.pic2coordx=60;
+$scope.pic2height=30;
+$scope.pic2coordy=48;
+$scope.pic2coordx=56;
 
 $scope.smallimage2=function()
 {
-$scope.pic2height=25; $scope.pic2coordx=60; $scope.pic2coordy=50;	
+$scope.pic2height=30; $scope.pic2coordx=56; $scope.pic2coordy=48;	
 }
 $scope.bigimage2=function()
 {
