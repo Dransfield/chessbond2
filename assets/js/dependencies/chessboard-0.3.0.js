a3296abb49ad123e40ec24f900fb06dc10dcaa98
root <slenkar@gmail.com>
2016-12-22 17:16:44 +0800

    h

--- assets/js/dependencies/chessboard-0.3.0.js
+++ assets/js/dependencies/chessboard-0.3.0.js
@@ -665,9 +665,11 @@ function buildPiece(piece, hidden, id) {
   'class="' + CSS.piece + '" ' +
   'data-piece="' + piece + '" ' +
   'style="width: ' + ((SQUARE_SIZE/100)*scale) + 'px;' +
-	'vertical-align: middle;'+
-	' margin-left: auto;'+
-    'margin-right: auto;'+
+	' position: absolute;'+
+    'top: 50%;'+
+    'left: 50%;'+
+    'margin-right: -50%;'+
+    'transform: translate(-50%, -50%);'+
   'height: ' + ((SQUARE_SIZE/100)*scale) + 'px;';
   if (hidden === true) {
     html += 'display:none;';
