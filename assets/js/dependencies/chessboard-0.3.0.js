ece77d87344a4e4529ab2cb2fc16e5665811aaac
root <slenkar@gmail.com>
2016-12-22 17:26:18 +0800

    h

--- assets/js/dependencies/chessboard-0.3.0.js
+++ assets/js/dependencies/chessboard-0.3.0.js
@@ -1195,8 +1195,8 @@ function beginDraggingPiece(source, piece, x, y) {
       width: wid+'px',
       height:hei+'px',
       position: 'absolute',
-      left: x+wid ,
-      top: y+hei
+      left: x+(wid*2) ,
+      top: y+(hei*2)
     });
 
   if (source !== 'spare') {
