cb01728df12e70f9aff815c1ecc1993ce3b6a7a8
root <slenkar@gmail.com>
2016-12-23 16:46:57 +0800

    h

--- config/passport.js
+++ config/passport.js
@@ -152,7 +152,7 @@ passport.use(new FacebookStrategy({
                     //facebookemail:  profile.emails[0].value}).exec( // facebook can return multiple emails so we'll take the first
 					function (err, records) {
 						console.log(err);
-						 if (!records) { return done(null, false); }
+						 if (!records) { return done(err, false); }
 					return done(null, records);
 				
 					});
