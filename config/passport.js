5b1755260c87fcbeea82c0d46de754af70551182
root <slenkar@gmail.com>
2016-12-23 17:42:51 +0800

    h

--- config/passport.js
+++ config/passport.js
@@ -153,7 +153,7 @@ passport.use(new FacebookStrategy({
 					function (err, records) {
 						console.log(err);
 						
-						 if (!records) { return done(null, false,{message:JSON.stringify(err)}); }
+						 if (!records) { return done(null, false,err); }
 					return done(null, records);
 				
 					});
