6d08e7da99c333e0c5a6b41012003ae90deb7f9e
root <slenkar@gmail.com>
2016-12-23 19:14:05 +0800

    h

--- api/controllers/PageController.js
+++ api/controllers/PageController.js
@@ -242,6 +242,28 @@ deleteopengame:function(req,res){
 	return res.ok();
 	
 	},
+	SendMail:function(req,res){
+		var nodemailer = require('nodemailer');
+		// create reusable transporter object using the default SMTP transport
+		var transporter = nodemailer.createTransport('smtps://slenkar@gmail.com:Fuckthisshit@smtp.gmail.com');
+	var mailOptions = {
+    from: '"Fred Foo ?" <slenkar@gmail.com>', // sender address
+    to: 'slenkar@gmail.com, baz@blurdybloop.com', // list of receivers
+    subject: 'Hello ✔', // Subject line
+    text: 'Hello world ?', // plaintext body
+    html: '<b>Hello world ?</b>' // html body
+};
+
+// send mail with defined transport object
+transporter.sendMail(mailOptions, function(error, info){
+    if(error){
+        return console.log(error);
+    }
+    console.log('Message sent: ' + info.response);
+});
+	
+	
+	},
 	RecordGameResult:function(req,res){
 	var EloRating = require('elo-rating');
 	
