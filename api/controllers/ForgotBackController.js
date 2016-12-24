c110a46d662da1870dcaba2e69f65e9935b7a105
root <slenkar@gmail.com>
2016-12-24 16:24:44 +0800

    h

--- api/controllers/ForgotBackController.js
+++ api/controllers/ForgotBackController.js
@@ -10,16 +10,17 @@ module.exports = {
 		var nodemailer = require('nodemailer');
 		// create reusable transporter object using the default SMTP transport
 		var transporter = nodemailer.createTransport('smtps://slenkar@gmail.com:Fuckthisshit@smtp.gmail.com');
+	 var code=md5(Date.now()+user.id);
+    
+    user.passwordcode =code;
+    user.save();
+    
 	var mailOptions = {
     from: '"Prakash" <admin@chessbond.com>', // sender address
     to: req.param('address'), // list of receivers
     subject: 'Forgotten Password', // Subject line
     text: 'Here is your link to reset your password:', // plaintext body
-    md5=require("MD5");
-    var code=md5(Date.now()+user.id);
     
-    user.passwordcode =code;
-    user.save();
     html: "'<a href='http://www.chessbond/com/forgot/password/'+code+'</a>'" // html body
 };
 
