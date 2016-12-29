ab42631b1141e951645faf3e8e0a5007c6967beb
root <slenkar@gmail.com>
2016-12-29 20:18:59 +0800

    h

--- assets/js/TwoPlayerController.js
+++ assets/js/TwoPlayerController.js
@@ -204,7 +204,7 @@ $scope.pic2height=200; $scope.pic2coordx=0;	$scope.pic2coordy=0;
 	};
 	$scope.PlayMove=function()
     {
-		console.log("play move sound");
+		console.log("play move sound" +$scope.MoveSound);
 	$scope.MoveSound.play();
 	};
 	$scope.PlayCheckMate=function()
