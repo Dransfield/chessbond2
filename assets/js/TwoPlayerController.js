7ceb442ade51193c7ac5c2afd93d5deb2c4a8340
root <slenkar@gmail.com>
2017-01-01 17:37:33 +0800

    h

--- assets/js/TwoPlayerController.js
+++ assets/js/TwoPlayerController.js
@@ -59,7 +59,7 @@ $scope.BlackInterval=0;
     ];
     
   $scope.boardsizes = [
-      100,200,400,800
+      200,300,400,600
     ];
     $scope.piecevalues={P:1,N:3,B:3,R:5,Q:9};
     $scope.BellSound= new Audio('/alert.mp3');
