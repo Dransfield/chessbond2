1186dfd9a3a5404124722365761931c8b76f93d4
root <slenkar@gmail.com>
2017-01-01 20:35:05 +0800

    h

--- assets/js/TwoPlayerController.js
+++ assets/js/TwoPlayerController.js
@@ -230,6 +230,9 @@ $scope.pic2height=200; $scope.pic2coordx=0;	$scope.pic2coordy=0;
 			.then(function onSuccess(sailsResponse){
 			$scope.User=sailsResponse.data;
 			$scope.setBoard(MyID);
+			if (!$scope.User.BoardSize)
+			{$scope.User.BoardSize==400;}
+
 			if ($scope.User.BoardSize==0)
 			{$scope.User.BoardSize==400;}
       		}
