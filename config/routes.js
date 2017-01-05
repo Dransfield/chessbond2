3d757d746713dc1011b4b44a2c89662e7bcd86a3
root <slenkar@gmail.com>
2017-01-05 14:35:50 +0800

    h

--- config/routes.js
+++ config/routes.js
@@ -41,6 +41,7 @@ module.exports.routes = {
 	//'GET /HomepageHeartbeat':'PageController.HomepageHeartbeat',
 	
 	'GET /humanvshuman/:GameID':{view:'humanvshuman'},
+	'GET /humanvshumannew/:GameID':{view:'humanvshumannew'},
 	'/MyLogout':'AuthController.logout',
 	'PUT /joingame':'PageController.Joingame',
 	'PUT /SendMail':'ForgotBackController.SendMail',
