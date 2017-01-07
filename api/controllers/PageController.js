6496c4a5644b3d5d47e989844d64a5d7f6c64a2a
root <slenkar@gmail.com>
2017-01-07 19:50:27 +0800

    h

--- api/controllers/PageController.js
+++ api/controllers/PageController.js
@@ -356,7 +356,7 @@ deleteopengame:function(req,res){
 */
 	updateGameTime:function(req,res){
 	Chessgame.findOne(req.param('GameID'), function foundChessgame(err, cg) {
-		If(cg)
+		if(cg)
 		{
 		if(cg.Player1==req.param('player'))
 		{}
