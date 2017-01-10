d3fa9e94dbf0516a5328aa708d7c146779c1914a
root <slenkar@gmail.com>
2017-01-10 14:29:28 +0800

    h

--- api/controllers/PageController.js
+++ api/controllers/PageController.js
@@ -5,67 +5,9 @@
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
  
-var SMTPServer = require('smtp-server').SMTPServer;
-var options={onData:maildata};
-var server = new SMTPServer(options);
 
-server.listen(110);
 
-var MailListener = require("mail-listener2");
 
-var mailListener = new MailListener({
-  username: "imap-username",
-  password: "imap-password",
-  host: "localhost",
-  port: 110, // imap port
-  tls: true,
-  connTimeout: 10000, // Default by node-imap
-  authTimeout: 5000, // Default by node-imap,
-  debug: console.log, // Or your custom function with only one incoming argument. Default: null
-  tlsOptions: { rejectUnauthorized: false },
-  mailbox: "INBOX", // mailbox to monitor
-  searchFilter: ["UNSEEN", "FLAGGED"], // the search filter being used after an IDLE notification has been retrieved
-  markSeen: true, // all fetched email willbe marked as seen and not fetched next time
-  fetchUnreadOnStart: true, // use it only if you want to get all unread email on lib start. Default is `false`,
-  mailParserOptions: {streamAttachments: true}, // options to be passed to mailParser lib.
-  attachments: true, // download attachments as they are encountered to the project directory
-  attachmentOptions: { directory: "attachments/" } // specify a download directory for attachments
-});
-
-mailListener.start(); // start listening
-
-// stop listening
-//mailListener.stop();
-
-mailListener.on("server:connected", function(){
-  console.log("imapConnected");
-});
-
-mailListener.on("server:disconnected", function(){
-  console.log("imapDisconnected");
-});
-
-mailListener.on("error", function(err){
-  console.log(err);
-});
-
-mailListener.on("mail", function(mail, seqno, attributes){
-  // do something with mail object including attachments
-  console.log("emailParsed", mail);
-  // mail processing code goes here
-});
-
-mailListener.on("attachment", function(attachment){
-  console.log(attachment.path);
-});
-
-function maildata(stream, session, callback)
-{
-	stream.pipe('hi.txt');
-	
-	  stream.pipe(process.stdout); // print message to console
-        stream.on('end', callback);
-	}
 
 // it's possible to access imap object from node-imap library for performing additional actions. E.x.
 
