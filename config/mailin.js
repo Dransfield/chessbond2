71c8f89a0e5fa421bff9fbc7643d7953d97677d5
root <slenkar@gmail.com>
2017-01-10 11:43:24 +0800

    h

--- config/mailin.js
+++ config/mailin.js
@@ -1,6 +1,6 @@
 // Other mailin options, @see https://github.com/Flolagale/mailin/blob/master/lib/mailin.js#L26
 module.exports.mailin = {
 	enable: true,
-	port:993 ,
+	port:143 ,
 	handlerService: 'MailinService'
 }
