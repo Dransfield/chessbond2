3382e03039369a4b8e60726029eed472fa4160c7
root <slenkar@gmail.com>
2017-01-17 12:45:08 -0500

    h

--- config/ssl.js
+++ config/ssl.js
@@ -1,18 +1,21 @@
-module.exports = {
-files = [
+var files = [
   "COMODORSADomainValidationSecureServerCA.crt",
   
   "COMODORSAAddTrustCA.crt",
   "AddTrustExternalCARoot.crt"
 ]
-
+var car;
 for (file in files)
 {
 car =car+ (require('fs').readFileSync(require('path').resolve(__dirname,'ssl'+file )
 }
+
+module.exports = {
+
+
 ssl: {
   //   ca: require('fs').readFileSync(require('path').resolve(__dirname,'ssl/bundle.crt')),
-      ca: ca,
+      ca: car,
    
      key: require('fs').readFileSync(require('path').resolve(__dirname,'ssl/chessbond.com.key')),
     cert: require('fs').readFileSync(require('path').resolve(__dirname,'ssl/www_chessbond_com.crt'))
