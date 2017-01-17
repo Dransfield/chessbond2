4b6f90c1da10199dea75cbb007860ddadfe976d5
root <slenkar@gmail.com>
2017-01-17 15:08:50 -0500

    h

--- assets/js/TwoPlayerController.js
+++ assets/js/TwoPlayerController.js
@@ -207,7 +207,8 @@ $scope.StartBlackClock=function()
 		var bythousand=$scope.BlackTime/1000;
 		$scope.BlackSeconds=(parseInt((bythousand % 60))).toString();
 		$scope.BlackMinutes=(parseInt((bythousand/60))).toString();
-		$scope.BlackMilliseconds=(parseInt(($scope.BlackTime % 1000))).toString();
+		$scope.BlackMilliseconds=(parseInt(($scope.BlackTime % 1000)));
+		$scope.BlackMilliseconds=$scope.BlackMilliseconds.toString();
 		if (parseInt($scope.BlackSeconds<10))
 		{$scope.BlackSeconds="0"+$scope.BlackSeconds;}
 		if (parseInt($scope.BlackMilliSeconds)<100 && parseInt($scope.BlackMilliSeconds)>10)
