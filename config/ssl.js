ec2d298342e438cd6c7ab951bdfa27942c615a55
root <slenkar@gmail.com>
2017-01-17 13:15:34 -0500

    h

--- config/ssl.js
+++ config/ssl.js
@@ -20,7 +20,7 @@ module.exports = {
 
 ssl: {
      //ca: require('fs').readFileSync(require('path').resolve(__dirname,'ssl/bundle.crt')),
-      ca: files,
+      ca: files.join(),
    
      key: require('fs').readFileSync(require('path').resolve(__dirname,'ssl/chessbond.com.key')),
     cert: require('fs').readFileSync(require('path').resolve(__dirname,'ssl/www_chessbond_com.crt'))
