0090dc3dc72fa9af31fc518c23290598b40ce8e0
root <slenkar@gmail.com>
2017-01-27 18:56:49 -0500

    h

--- assets/js/ProfileController.js
+++ assets/js/ProfileController.js
@@ -29,7 +29,7 @@ for (x in $scope.MyGames)
 {
 
 if ($scope.MyGames[x].Player1Color=='White')
-{$scope.GameInfo.push({WhitePlayerID:$scope.MyGames[x].Player1,BlackPlayerID:$scope.MyGames[x].Player2,WhitePlayerName:$scope.MyGames[x].Player1Name,BlackPlayerName:$scope.MyGames[x].Player2Name})}
+{$scope.GameInfo.push({WhitePlayerID:$scope.MyGames[x].Player1,BlackPlayerID:$scope.MyGames[x].Player2,WhitePlayerName:$scope.MyGames[x].Player1Name,BlackPlayerName:$scope.MyGames[x].Player2Name});}
 else
 {}
 	
