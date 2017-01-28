e98b60d6c2fa0ea549602ee9453deb706f87f862
root <slenkar@gmail.com>
2017-01-27 22:01:02 -0500

    h

--- assets/js/TwoPlayerController.js
+++ assets/js/TwoPlayerController.js
@@ -1664,7 +1664,7 @@ $scope.currentFavicon=src;
 		
 			$timeout(function(){
 				board1.resize();
-				$scope.colorBoard(me,true);},10000);
+				$scope.colorBoard(me,true);},0);
 		
 		};
 		$scope.resetBoard=function(me)
