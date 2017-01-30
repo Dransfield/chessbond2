4081a9801ef99d052920f1583c28416efc624fb9
root <slenkar@gmail.com>
2017-01-30 10:52:57 -0500

    h

--- api/controllers/AuthController.js
+++ api/controllers/AuthController.js
@@ -20,7 +20,7 @@ passport.authenticate('google',{ scope: ['profile','email'] })(req, res);
 	},
 	googlecallback:function(req,res)
 	{
-		console.log("REQ"+JSON.stringify(req));
+		//console.log("REQ"+JSON.stringify(req));
 		console.log("RES"+JSON.stringify(res));
 		 passport.authenticate('google',{
             successRedirect : '/profile',
