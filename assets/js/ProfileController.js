a8cb655160c83de0c965c0d99746ac410daf385d
root <slenkar@gmail.com>
2017-01-30 18:03:14 -0500

    h

--- assets/js/ProfileController.js
+++ assets/js/ProfileController.js
@@ -432,7 +432,7 @@ $scope.countries=[
 		$scope.PrefSelectChanged=function(pref,me,func)
 	{
 		$scope.ChangePreference(pref,me,$scope.User[pref]);
-		$scope.$apply($scope.User[pref]);
+		$scope.$apply($scope.User[pref]=$scope.User[pref]);
 		console.log("changed "+pref+" to "+JSON.stringify($scope.User[pref]));
 		if(func){
 		func(me);}
