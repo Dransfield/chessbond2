262237ade449bd4c7c3facb1680f2f00b226a538
root <slenkar@gmail.com>
2017-01-30 18:31:14 -0500

    h

--- assets/js/TwoPlayerController.js
+++ assets/js/TwoPlayerController.js
@@ -379,12 +379,12 @@ $scope.StartBlackClock=function()
     
 
 $scope.pic1height=30;
-$scope.pic1coordy=35;
+$scope.pic1coordy=45;
 $scope.pic1coordx=30;
 
 $scope.smallimage1=function()
 {
-$scope.pic1height=30; $scope.pic1coordx=30; $scope.pic1coordy=35;	
+$scope.pic1height=30; $scope.pic1coordx=30; $scope.pic1coordy=45;	
 }
 $scope.bigimage1=function()
 {
