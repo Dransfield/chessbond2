e03385fb2a0000917cb994894c376873ecfdaa61
root <slenkar@gmail.com>
2017-02-07 12:15:34 -0500

    hi

--- assets/js/ProfileController.js
+++ assets/js/ProfileController.js
@@ -127,7 +127,7 @@ $scope.chessgameskip=0;
 			});
 			});
 	};
-	$scope.SendWallPostReply(user,text,replyingto)
+	$scope.SendWallPostReply=function(user,text,replyingto)
 	{
 			$http.post("/newwallpost",{ReplyTo:replyingto,senderpic:$scope.User.picture,content:text,sender:$scope.User.id,sendername:$scope.User.name,roomName:$scope.LookedatUser.id,reciever:$scope.LookedatUser.id})
 			.then(function onSuccess (){
