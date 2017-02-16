a46afee72bf95e95639018cfcd50afcbd62b176f
root <slenkar@gmail.com>
2017-02-16 11:08:16 -0500

    hi

--- api/controllers/PageController.js
+++ api/controllers/PageController.js
@@ -230,7 +230,7 @@ module.exports = {
 		
 	
 	user.Numberoftimesloggedin+=1;
-	var dateObj=Date.now();
+	var dateObj=new Date();
 	var month = dateObj.getUTCMonth() + 1; //months from 1-12
 var day = dateObj.getUTCDate();
 var year = dateObj.getUTCFullYear();
