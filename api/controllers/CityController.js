6f3217249aa5dabad8baf9deb44e111fe7b0c17d
root <slenkar@gmail.com>
2017-02-21 14:27:29 -0500

    hi

---
+++ api/controllers/CityController.js
@@ -0,0 +1,20 @@
+/**
+ * CityController
+ *
+ * @description :: Server-side logic for managing Cities
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	CreateDatabase:function(req,res){
+		
+		var lineReader = require('readline').createInterface({
+  input: require('fs').createReadStream('https://www.chessbond.com/worldcitiespop.txt')
+});
+
+lineReader.on('line', function (line) {
+  console.log('Line from file:', line);
+});
+	}
+};
+
