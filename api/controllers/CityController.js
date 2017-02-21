09f486bbfbdeee5e35edb3a70741678eb0fa11bb
root <slenkar@gmail.com>
2017-02-21 18:20:15 -0500

    hi

--- api/controllers/CityController.js
+++ api/controllers/CityController.js
@@ -32,10 +32,84 @@ lr.on('line', function (line) {
 
 lr.on('end', function () {
 	// All lines are read, file is closed now.
+	
+	 lr = new LineByLineReader('/home/chessbond/chessbond/assets/worldcitiespop2.txt');
+
+lr.on('error', function (err) {
+	// 'err' contains error object
 });
 
+lr.on('line', function (line) {
+	// pause emitting of lines...
+	lr.pause();
+	var myarray=line.split(",");
+	City.create({country:myarray[0],name:myarray[1] }).exec(function (err, records) {
+	 
+	});
+	// ...do your asynchronous line processing..
+	setTimeout(function () {
+
+		// ...and continue emitting lines.
+		lr.resume();
+	}, 100);
+});
+
+lr.on('end', function () {
+	 lr = new LineByLineReader('/home/chessbond/chessbond/assets/worldcitiespop3.txt');
+
+lr.on('error', function (err) {
+	// 'err' contains error object
+});
+
+lr.on('line', function (line) {
+	// pause emitting of lines...
+	lr.pause();
+	var myarray=line.split(",");
+	City.create({country:myarray[0],name:myarray[1] }).exec(function (err, records) {
+	 
+	});
+	// ...do your asynchronous line processing..
+	setTimeout(function () {
+
+		// ...and continue emitting lines.
+		lr.resume();
+	}, 100);
+});
+
+lr.on('end', function () {
+	 lr = new LineByLineReader('/home/chessbond/chessbond/assets/worldcitiespop4.txt');
+
+lr.on('error', function (err) {
+	// 'err' contains error object
+});
+
+lr.on('line', function (line) {
+	// pause emitting of lines...
+	lr.pause();
+	var myarray=line.split(",");
+	City.create({country:myarray[0],name:myarray[1] }).exec(function (err, records) {
+	 
+	});
+	// ...do your asynchronous line processing..
+	setTimeout(function () {
+
+		// ...and continue emitting lines.
+		lr.resume();
+	}, 100);
+});
+
+lr.on('end', function () {
+	// All lines are read, file is closed now.
+});
   
+});
   
+});
+  
+	
+});
+
+     
 
 	});
 }
