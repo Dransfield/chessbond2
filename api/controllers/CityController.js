4cc5576de96d715d7f495d621b8213fd8f7b13ba
root <slenkar@gmail.com>
2017-02-21 18:09:20 -0500

    hi

--- api/controllers/CityController.js
+++ api/controllers/CityController.js
@@ -7,35 +7,41 @@
 
 module.exports = {
 	CreateDatabase:function(req,res){
-		var fs = require('fs');
-		fs.readdir("/home/chessbond/chessbond/assets", function(err,files){
+		//var fs = require('fs');
+		/*fs.readdir("/home/chessbond/chessbond/assets", function(err,files){
 			
 			console.log("about to read files");
 			console.log(files)});
-	var stream=fs.ReadStream('/home/chessbond/chessbond/assets/readthis.txt', {start: 0, end: 99});
+	}*/
+	//var stream=fs.ReadStream('/home/chessbond/chessbond/assets/readthis.txt', {start: 0, end: 99});
+var LineByLineReader = require('line-by-line'),
+    lr = new LineByLineReader('/home/chessbond/chessbond/assets/readthis.txt');
 
-stream.on('readable', function() {
-    var data = stream.read();
-    console.log(data);
-});
- 
-stream.on('end', function() {
-    console.log("THE END");
-});
-		/*var lineReader = require('readline').createInterface({
-  input: require('fs').createReadStream('/home/chessbond/chessbond/assets/worldcitiespop.txt')
+lr.on('error', function (err) {
+	// 'err' contains error object
 });
 
-lineReader.on('line', function (line) {
-  //console.log('Line from file:', line);
-  var myarray=line.split(",");
-  lineReader.pause();
-  City.create({country:myarray[0],city:myarray[1] }).exec(function (err, records) {
-	lineReader.resume();
+lr.on('line', function (line) {
+	// pause emitting of lines...
+	lr.pause();
+	City.create({country:line[0],name:line[1] }).exec(function (err, records) {
 	 
 	});
+	// ...do your asynchronous line processing..
+	setTimeout(function () {
+
+		// ...and continue emitting lines.
+		lr.resume();
+	}, 100);
+});
+
+lr.on('end', function () {
+	// All lines are read, file is closed now.
+});
+
+  
   
 });*/
-	}
+	
 };
 
