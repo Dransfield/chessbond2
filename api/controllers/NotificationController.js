6ffdb1ea5ae2e3fd1207969484386a3806f459a8
root <slenkar@gmail.com>
2017-02-24 15:45:45 -0500

    h

--- api/controllers/NotificationController.js
+++ api/controllers/NotificationController.js
@@ -2,7 +2,7 @@ module.exports = {
 
 		newnotificaiton:function(req,res){
 			
-	notification.create({reciever:req.param('reciever'),msg:req.param('msg'),adr:req.param('adr')}.exec(function (err, records) {
+	notification.create({reciever:req.param('reciever'),msg:req.param('msg'),adr:req.param('adr')}).exec(function (err, records) {
 	sails.sockets.broadcast(records.reciever,'notification', records);
 	
 	 return res.ok();
