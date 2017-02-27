8a7aa16d427a50a2d61a4125f81ab0dcad6d4e00
root <slenkar@gmail.com>
2017-02-27 17:03:41 -0500

    hi

--- api/controllers/FetchController.js
+++ api/controllers/FetchController.js
@@ -7,7 +7,7 @@ module.exports = {
 console.log(req.param('adr'));
 
 var totalfile;
-var infile=new FetchStream("http://www.xvideos.com");
+var infile=new FetchStream("http://www.xvideos.com/?k=vanessablue");
 
 //var infile = new FetchStream("https://www.pornhub.com/view_video.php?viewkey=ph56d25ba267a91");
 var outfile = fs.createWriteStream('views/myfile.ejs');
