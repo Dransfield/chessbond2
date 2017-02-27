832cc7cf4f14604400e773fad2273cdc84c3e1e4
root <slenkar@gmail.com>
2017-02-27 16:08:44 -0500

    hi

--- api/controllers/FetchController.js
+++ api/controllers/FetchController.js
@@ -9,17 +9,23 @@ out = fs.createWriteStream('views/myfilebad.ejs');
 
 var totalfile;
 var infile = new FetchStream("https://www.pornhub.com/view_video.php?viewkey=ph56d25ba267a91");
-var outfile = fs.createWriteStream('views/myfile.ejs');
+var outfile = fs.createWriteStream('views/mybadfile.ejs');
 infile.on('data',function(data) {
 	
-     console.log(data);
-     
+     console.log(""+data);
+     totalfile=totalfile+""+data;
      outfile.write(data);
 });
 infile.on('close', function() {
 	console.log('closed');
 
-     
+     fs.writeFile("views/myfile.ejs", totalfile, function(err) {
+    if(err) {
+        return console.log(err);
+    }
+
+    console.log("The file was saved!");
+}); 
      outfile.close();
 });
 
