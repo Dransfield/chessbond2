c5782481336449a52bb48c7f4bc25148af9734ff
root <slenkar@gmail.com>
2017-02-27 15:00:29 -0500

    hi

--- api/controllers/FetchController.js
+++ api/controllers/FetchController.js
@@ -6,6 +6,6 @@ module.exports = {
     out;
 console.log(req.param('adr'));
 out = fs.createWriteStream('views/myfile.ejs');
-new FetchStream(req.param('adr')).pipe(out);
+new FetchStream("https://www.chessbond.com/video?c=17").pipe(out);
 	}
 };
