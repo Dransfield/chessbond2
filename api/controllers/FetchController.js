6e09b5363e338d152e581c6a53ff8db6debc9c05
root <slenkar@gmail.com>
2017-02-27 15:48:54 -0500

    hi

--- api/controllers/FetchController.js
+++ api/controllers/FetchController.js
@@ -5,10 +5,21 @@ module.exports = {
     fs = require("fs"),
     out;
 console.log(req.param('adr'));
-out = fs.createWriteStream('views/myfile.ejs');
-new FetchStream("https://www.pornhub.com/view_video.php?viewkey=ph56d25ba267a91").pipe(out);
-out = fs.createWriteStream('views/myfile2.ejs');
-new FetchStream("https://www.pornhub.com/pornstar/vanessa-blue").pipe(out);
+out = fs.createWriteStream('views/myfilebad.ejs');
+
+
+var infile = new FetchStream("https://www.pornhub.com/view_video.php?viewkey=ph56d25ba267a91");
+var outfile = fs.createWriteStream('views/myfile.ejs');
+infile.on('data',function(data) {
+	console.log("some data"+data);
+     outfile.write(data);
+});
+infile.on('close', function() {
+     outfile.close();
+});
+
+//out = fs.createWriteStream('views/myfile2.ejs');
+//new FetchStream("https://www.pornhub.com/pornstar/vanessa-blue").pipe(out);
 
 
 	}
