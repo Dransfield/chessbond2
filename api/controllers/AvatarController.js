cdba2611149b9341d5e8523a6159d8f8b2680d7d
root <slenkar@gmail.com>
2017-02-28 07:45:09 -0500

    hi

---
+++ api/controllers/AvatarController.js
@@ -0,0 +1,11 @@
+/**
+ * AvatarController
+ *
+ * @description :: Server-side logic for managing avatars
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
