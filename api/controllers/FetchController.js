347a383e5c16f576c3e431480c49e637bfb65524
root <slenkar@gmail.com>
2017-03-01 13:34:43 -0500

    hi

--- api/controllers/FetchController.js
+++ api/controllers/FetchController.js
@@ -99,7 +99,7 @@ var totalfile;
 //var infile=new FetchStream("http://jesuits.org/aboutus");
 //var infile=new FetchStream("http://www.pornhub.com/video/search?search=ebony+teen");
 //https://www.chessbond.com/video/search?search=ebony+tittyfuck&page=2
-var infile=new FetchStream("http://www.xvideos.com/?k=ms+deja");
+var infile=new FetchStream("http://www.xvideos.com/?k=miss+deja&p=1");
 var outfile = fs.createWriteStream('views/myfile.ejs');
 infile.on('data',function(data) {
 	
