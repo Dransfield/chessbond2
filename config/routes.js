51138a8e36cabd22490b18bf0f4decb073477e66
root <slenkar@gmail.com>
2017-03-01 13:27:42 -0500

    hi

--- config/routes.js
+++ config/routes.js
@@ -42,7 +42,7 @@ module.exports.routes = {
 	'/forgot':{view:'forgot'},
 	'/myfile': {view: 'myfile'},
 	//'/myfile2': {view: 'myfile2'},
-	//'/tournament':{view:'go'},
+	'/tournament':{view:'go'},
 	'GET /registerpage':{view:'register'},
 	'GET /getmyfile':'FetchController.GetFile',
 	//'GET /HomepageHeartbeat':'PageController.HomepageHeartbeat',
