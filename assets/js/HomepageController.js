83126476a89c7152166048b31f729f7184f41028
root <slenkar@gmail.com>
2017-03-07 07:53:09 -0500

    hi

--- assets/js/HomepageController.js
+++ assets/js/HomepageController.js
@@ -10,7 +10,28 @@ $scope.GameForm.timelimit=5;
 $scope.GameForm.color='White';
 $scope.SoleConnectorVariable="";
 	//document.getElementById("body").style.backgroundColor="#4D129F";
-	
+		$scope.gamecategories=[{time:1,extratime:0},
+					{time:2,extratime:0},
+					{time:3,extratime:0},
+					{time:4,extratime:0},
+					{time:5,extratime:0},
+					{time:6,extratime:0},
+					{time:7,extratime:0},
+					{time:8,extratime:0},
+					{time:9,extratime:0},
+					{time:10,extratime:0},
+					{time:15,extratime:0},
+					{time:20,extratime:0},
+					{time:30,extratime:0},
+					{time:60,extratime:0},
+					{time:2,extratime:1},
+					{time:3,extratime:1},
+					{time:5,extratime:2},
+					{time:10,extratime:5},
+					{time:15,extratime:5},
+					{time:20,extratime:10},
+					{time:30,extratime:10},
+					{time:60,extratime:10}]
 	
 	$scope.GetNotifications=function(usrid)
 	{
