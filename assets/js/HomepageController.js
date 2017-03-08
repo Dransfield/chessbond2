52b2262201bebbe91caba2807937720481565f26
root <slenkar@gmail.com>
2017-03-07 20:02:34 -0500

    hi

--- assets/js/HomepageController.js
+++ assets/js/HomepageController.js
@@ -108,7 +108,7 @@ $scope.SoleConnectorVariable="";
 		console.log("$scope.GameForm1`"+$scope.timeobject['time']);
 		console.log("$scope.GameForm2"+$scope.timeobject.time);
 		console.log("$scope.GameForm2"+$scope.timeobject.extratime);
-	io.socket.put('/newopengame', { GameType:type,TimeLimit:$scope.GameForm.timeobject.time,ExtraTimeLimit:$scope.GameForm.timeobject.extratime,Player1Color:$scope.GameForm.color,Player1: id,Player1Name:name },
+	io.socket.put('/newopengame', { GameType:type,TimeLimit:$scope.timeobject.time,ExtraTimeLimit:$scope.timeobject.extratime,Player1Color:$scope.GameForm.color,Player1: id,Player1Name:name },
     function (resData, jwr) {
 
       // Refresh the page now that we've been logged in.
