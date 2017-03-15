f36e3a72274022b05a571fcfdbd0b1a8f9e2a24b
slenkar <slenkar@gmail.com>
2017-03-15 14:36:22 -0400

    hi

--- api/models/User.js
+++ api/models/User.js
@@ -27,8 +27,8 @@ BoardSize:{type:'integer',defaultsTo:300},
    Lastlogin:{type:'string',defaultsTo:""},
    CurrentCity:{type:'string',defaultsTo:""},
    FideTitle:{type:'string',defaultsTo:""},
-   ValidFideID:{type:'string',defaultsTo:""},
-   FideRatings:{type:'string',defaultsTo:""}
+   ValidFideID:{type:'integer',defaultsTo:0},
+   FideRatings:{type:'integer',defaultsTo:0}
 
 },
   beforeCreate: function(user, cb) {
