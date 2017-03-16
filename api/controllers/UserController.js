3b9a9e5627c40b00f30286b4a4ec9673d51fc0c8
slenkar <slenkar@gmail.com>
2017-03-16 10:39:14 -0400

    hi

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -26,6 +26,13 @@ module.exports = {
 				if(req.session.passport.user){
 						 User.update({id:req.session.passport.user},{Invisible:true}).
 						 exec(function afterwards(err, updated){
+							 var date =Date.now()+2000;
+							 //var date = new Date(2012, 11, 21, 5, 30, 0);
+
+								var j = schedule.scheduleJob(date, function(){
+								console.log('The world is going to end today.');
+								});
+							 
 						 res.redirect("/DeletedAccount");
 						 });
 						 
