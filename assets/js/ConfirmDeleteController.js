af7a1233e0c30e29b611f8cd88c7869141b693cf
slenkar <slenkar@gmail.com>
2017-03-16 16:25:13 -0400

    hi

---
+++ assets/js/ConfirmDeleteController.js
@@ -0,0 +1,33 @@
+angular.module('HomepageModule').controller('ConfirmDeleteController', ['$scope', '$http','$window', 'toastr', function($scope, $http,$window, toastr){
+	
+	$scope.confirmdelete=function()
+	{
+	console.log(vm.password);
+	}
+	$scope.getuser=function(MyID)
+	{
+		$http.get('/user?id='+MyID, {
+			})
+			.then(function onSuccess(sailsResponse){
+			$scope.User=sailsResponse.data;
+			/*
+			for(x in $scope.categories)
+			{
+				for(c in $scope.colors)
+				{
+					if(!$scope.User['rating'+$scope.colors[c].col+$scope.categories[x].time+"|"+$scope.categories[x].extratime])
+					{
+						$scope.User['rating'+$scope.colors[c].col+$scope.categories[x].time+"|"+$scope.categories[x].extratime]=1200;
+						console.log('rating'+$scope.colors[c].col+$scope.categories[x].time+"|"+$scope.categories[x].extratime+$scope.User['rating'+$scope.colors[c].col+$scope.categories[x].time+"|"+$scope.categories[x].extratime]);}
+				}
+			}*/
+		});
+	};
+
+
+	
+	
+	
+
+	
+}]);
