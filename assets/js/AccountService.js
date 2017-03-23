fd2d577d876fedfc633a32a5ef45f157dae0a233
slenkar <slenkar@gmail.com>
2017-03-23 08:36:11 -0400

    hi

---
+++ assets/js/AccountService.js
@@ -0,0 +1,62 @@
+ angular.module('HomepageModule').service('Accounts', function () {
+        var Accounts = {};
+		var AccountsRequested={};
+        return {
+            getAccount: function (id) {
+                return Accounts[id];
+            },
+            setAccount: function(acc) {
+                Accounts[acc.id] = acc;
+            },
+            setField:function(accID,fieldName,fieldValue) {
+				Accounts[accID][fieldName]=fieldValue;
+			}
+            downloadAccount: function(accID) {
+                
+                      
+                console.log("is "+Myid+" account gotten?");
+	
+				if(!AccountsRequested[accID])
+				{
+					AccountsRequested[accID]=true;
+					console.log(accID+" not gotten");
+	
+					io.socket.get('/user/'+accID,
+					function(usr){
+					
+						if (usr)
+						{
+							setAccount(usr);
+								io.socket.get('/subscription?subscriber='+Myid,
+								function (rply) {
+		
+								
+									if(rply)
+									{
+									console.log("got reply");
+										if(rply.length>0)
+										{
+										console.log(accID+" is online");
+										setField(accID,'online',true);
+										}
+										else
+										{
+										
+										console.log(Myid+" is not online");
+										setField(accID,'online',false);
+										}
+				
+									}
+								
+							});
+						}
+					});
+	
+				}	
+                      
+                
+            }
+            
+            
+        };
+    });
