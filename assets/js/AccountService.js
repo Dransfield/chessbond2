745fc5c9d36ae311dceea17a7bf105bf723082b3
slenkar <slenkar@gmail.com>
2017-03-23 11:53:32 -0400

    hi

--- assets/js/AccountService.js
+++ assets/js/AccountService.js
@@ -1,7 +1,7 @@
  angular.module('HomepageModule').service('AccountService', function () {
         var Accounts = {};
 		var AccountsRequested={};
-		var Promises=[];
+		var Promises={};
 		function setAccount(acc){
 			     Accounts[acc.id] = acc;
            
