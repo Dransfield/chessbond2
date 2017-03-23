7b152fab7b7548669c1d11c85e9696e6295abfcf
slenkar <slenkar@gmail.com>
2017-03-23 12:21:38 -0400

    hi

--- assets/js/AccountController.js
+++ assets/js/AccountController.js
@@ -1,11 +1,16 @@
 angular.module('HomepageModule').controller('AccountController', ['$scope', '$http','$window' ,'toastr','AccountService', function($scope, $http,$window,toastr,AccountService){
 	
-	$scope.AccountService=AccountService;
-	$scope.AccountService.downloadAccounts();
+
+	$scope.addAccount=function(accID) {
+			AccountService.AccountsRequested[accID]=accID;
+			console.log(accID+" was requested");
+			
+			};
+	
+	AccountService.downloadAccounts();
 	console.log(JSON.stringify($scope.AccountService.getRequestedAccounts()));
 	Promise.all($scope.AccountService.getPromises()).then(values => { 
   console.log(values); 
   $scope.$apply(function(){});
 });
-	
 }]);
