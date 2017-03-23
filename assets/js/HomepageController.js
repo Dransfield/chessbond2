3809d114c1fdbd2925f843218d31d514cd3e5865
slenkar <slenkar@gmail.com>
2017-03-23 10:01:18 -0400

    hi

--- assets/js/HomepageController.js
+++ assets/js/HomepageController.js
@@ -331,7 +331,7 @@ $scope.PlayGame=function(GameID,Player2)
 $scope.joingame=function(GameID,PlayerID,PlayerName,playercolor,MyID,MyName,GamType,GamCategory,timelimit){
 		console.log("joingame player1"+PlayerID+" player2"+MyID+" player1name "+PlayerName+" Player2Name "+MyName);
 		console.log("timelimit*60"+timelimit*60);
-		if ($scope.User.Invisible==false)
+		if (!$scope.User.Invisible)
 		{
 		$http.put('/joingame', {
 			GameID:GameID,
