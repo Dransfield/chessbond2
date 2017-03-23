839bebbdbf014cb1516fade2755995681a85a4f9
slenkar <slenkar@gmail.com>
2017-03-23 00:25:59 -0400

    hi

--- assets/js/ProfileController.js
+++ assets/js/ProfileController.js
@@ -1,4 +1,4 @@
-angular.module('HomepageModule').controller('ProfileController', ['$scope', '$http','$window' ,'toastr', function($scope, $http,$window,toastr){
+angular.module('HomepageModule').controller('ProfileController', ['$scope', '$http','$window' ,'toastr','UsersFactory' function($scope, $http,$window,toastr,UsersFactory){
 $scope.User;
 $scope.SoleConnectorVariable="";
 $scope.MyGames=[];
