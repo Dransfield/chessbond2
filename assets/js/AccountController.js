e9639378780be5769bc0ad15757d494a4c2b804d
slenkar <slenkar@gmail.com>
2017-03-23 12:33:43 -0400

    hi

--- assets/js/AccountController.js
+++ assets/js/AccountController.js
@@ -6,8 +6,60 @@ angular.module('HomepageModule').controller('AccountController', ['$scope', '$ht
 			console.log(accID+" was requested");
 			
 			};
+	$scope.downloadAccounts= function() {
+                
+                      
+                for (x in AccountController.AccountsRequested)
+                {
+					Promises.push(new Promise((resolve,reject)=>{
+						
+				
+					
+					console.log(AccountController.AccountsRequested[x]+" not requested");
 	
-	AccountService.downloadAccounts();
+					io.socket.get('/user/'+AccountController.AccountsRequested[x],
+					function(usr){
+					
+						if (usr)
+						{
+							AccountController.setAccount(usr);
+								io.socket.get('/subscription?subscriber='+AccountController.AccountsRequested[x],
+								function (rply) {
+		
+								
+									if(rply)
+									{
+									console.log("got reply");
+										if(rply.length>0)
+										{
+										console.log(AccountController.AccountsRequested[x]+" is online");
+										AccountController.setField(AccountController.AccountsRequested[x],'online',true);
+										}
+										else
+										{
+										
+										console.log(AccountsRequested[x]+" is not online");
+										AccountController.setField(AccountController.AccountsRequested[x],'online',false);
+										}
+									resolve(AccountController.AccountsRequested[x]);
+									}
+									else
+									{resolve(AccountController.AccountsRequested[x]);}
+								
+							});
+						}
+						else
+						{resolve(AccountController.AccountsRequested[x]);}
+					});
+	
+				}));	
+                      
+                
+            }
+            
+            
+        }
+	$scope.downloadAccounts();
 	console.log(JSON.stringify(AccountService.getRequestedAccounts()));
 	Promise.all(AccountService.getPromises()).then(values => { 
   console.log(values); 
--- assets/js/AccountService.js
+++ assets/js/AccountService.js
@@ -36,62 +36,12 @@
 				}
 			},
 			addAccount:function(accID) {
+				if(!AccountsRequested[accID])
+			{
 			AccountsRequested[accID]=accID;
 			console.log(accID+" was requested");
-			
-			},
-            downloadAccounts: function() {
-                
-                      
-                for (x in AccountsRequested)
-                {
-					Promises.push(new Promise((resolve,reject)=>{
-						
-				
-					
-					console.log(AccountsRequested[x]+" not requested");
-	
-					io.socket.get('/user/'+AccountsRequested[x],
-					function(usr){
-					
-						if (usr)
-						{
-							setAccount(usr);
-								io.socket.get('/subscription?subscriber='+AccountsRequested[x],
-								function (rply) {
-		
-								
-									if(rply)
-									{
-									console.log("got reply");
-										if(rply.length>0)
-										{
-										console.log(AccountsRequested[x]+" is online");
-										setField(AccountsRequested[x],'online',true);
-										}
-										else
-										{
-										
-										console.log(AccountsRequested[x]+" is not online");
-										setField(AccountsRequested[x],'online',false);
-										}
-									resolve(AccountsRequested[x]);
-									}
-									else
-									{resolve(AccountsRequested[x]);}
-								
-							});
-						}
-						else
-						{resolve(AccountsRequested[x]);}
-					});
-	
-				}));	
-                      
-                
-            }
+			}
+			}
             
-            
-        }
     };
     });
