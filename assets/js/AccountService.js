3d05d5cca3b2f362b70fa9547907a305f0fd0633
slenkar <slenkar@gmail.com>
2017-03-23 12:01:50 -0400

    hi

--- assets/js/AccountService.js
+++ assets/js/AccountService.js
@@ -44,15 +44,15 @@
 						
 				
 					
-					console.log(accID+" not requested");
+					console.log(AccountsRequested[x]+" not requested");
 	
-					io.socket.get('/user/'+accID,
+					io.socket.get('/user/'+AccountsRequested[x],
 					function(usr){
 					
 						if (usr)
 						{
 							setAccount(usr);
-								io.socket.get('/subscription?subscriber='+accID,
+								io.socket.get('/subscription?subscriber='+AccountsRequested[x],
 								function (rply) {
 		
 								
@@ -61,24 +61,24 @@
 									console.log("got reply");
 										if(rply.length>0)
 										{
-										console.log(accID+" is online");
-										setField(accID,'online',true);
+										console.log(AccountsRequested[x]+" is online");
+										setField(AccountsRequested[x],'online',true);
 										}
 										else
 										{
 										
-										console.log(accID+" is not online");
-										setField(accID,'online',false);
+										console.log(AccountsRequested[x]+" is not online");
+										setField(AccountsRequested[x],'online',false);
 										}
-									resolve();
+									resolve(AccountsRequested[x]);
 									}
 									else
-									{resolve();}
+									{resolve(AccountsRequested[x]);}
 								
 							});
 						}
 						else
-						{resolve();}
+						{resolve(AccountsRequested[x]);}
 					});
 	
 				}));	
