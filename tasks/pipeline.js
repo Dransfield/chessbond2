48a4ea21258d2f2399259910fcdc0b1b5ead6127
slenkar <slenkar@gmail.com>
2017-03-24 21:11:15 -0400

    hi

--- tasks/pipeline.js
+++ tasks/pipeline.js
@@ -36,7 +36,7 @@ var jsFilesToInject = [
   // Dependencies like jQuery, or Angular are brought in here
   'js/dependencies/**/*.js',
   'js/HomepageModule.js',
-  'js/UsersFactory.js',
+  'js/AccountService.js',
 	
   // All of the rest of your client-side js files
   // will be injected here in no particular order.
