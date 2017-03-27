2b0cfb06cb7b757260e1859ae89b57af536760b0
slenkar <slenkar@gmail.com>
2017-03-26 23:48:42 -0400

    hi

--- assets/js/BlockedAccountService.js
+++ assets/js/BlockedAccountService.js
@@ -25,52 +25,19 @@
 			},
 		
 			getBlockedAccounts:function(accID){
-			console.log("get blocked accounts");	
-				for(var i = BlockedAccounts.length - 1; i >= 0; i--) {
-				console.log(BlockedAccounts[i]);
-			}
-				
 			return BlockedAccounts;
 			},
-		
-			
 			getRequestedBlockedAccounts:function() {
 			return BlockedAccountsRequested;
 			},
 			setRequestedBlockedAccounts:function() {
 			 BlockedAccountsRequested=true;
 			},
-		
-			clearBlockedAccountPromises:function(){
-			BlockedAccountPromises=[];
-			},
-			
-			getBlockedAccountPromises:function() {
-			return BlockedAccountPromises;
-			},
-			
-			addBlockedAccountPromise:function(prom){
-			BlockedAccountPromises.push(prom);
-			},
             setBlockedAccount: function(acc) {
-				console.log(acc +"is blocked ");
-				
-			
-				
                 BlockedAccounts[acc]=true;
-                
-               
-                
             }, 
             UnsetBlockedAccount: function(acc) {
-				console.log(acc +"is unblocked ");
-				
-			
-				
                 BlockedAccounts[acc]=false;
-                
-               
-                
             },
     
 			
--- assets/js/PrivateConversationController.js
+++ assets/js/PrivateConversationController.js
@@ -1,29 +1,64 @@
-angular.module('HomepageModule').controller('PrivateConversationController', ['$scope', '$http','$window' ,'toastr', function($scope, $http,$window,toastr){
+angular.module('HomepageModule').controller('PrivateconversationController', ['$scope', '$http','$window' ,'toastr','PrivateconversationService', function($scope, $http,$window,toastr,PrivateconversationService){
 
-//$scope.getuserAndJoinRoom=function(MyID)
-//	{
+$scope.Privateconversations=[];
 
-
-//};
-/*
-
-io.socket.get('/privateconversation?talkingto='+id,
-			function (msgs) {
-				//console.log(JSON.stringify(msgs));
-				
-				
-				console.log("msgs.length "+msgs.length);
-				for (x in msgs)
-				{
-				$scope.WallPosts[x]=msgs[x];
-				
-				msgs[x].Age=DateService.phrasefordate(msgs[x].createdAt);//$scope.CalcAge(msgs[x].createdAt);
-				
-						
-				}
-				
+$scope.DownloadedPrivateconversationsOnce=false;
+	$scope.setShouldGetPrivateconversations=function(accID){
+		PrivateconversationService.setShouldGetBlockedAccounts(accID);
+	$scope.downloadPrivateconversations();
+	};
+	
+	
+	$scope.NewPrivateconversation=function(MyID,sender)
+		{
 			
-			});
-*/
+		$scope.BlockedAccounts[sender]=true;
+			BlockedAccountService.setPrivateconversation(sender);
+			io.socket.post('/Privateconversation',{Talker1:MyID,Talker2:sender},
+			function (resData, jwRes) {
+				});
+		};
+		
+	$scope.downloadPrivateconversations= function() {
+			console.log("download Privateconversations");
+				if(PrivateconversationService.getShouldGetPrivateconversations()==true)
+				
+				{
+					if(PrivateconversationService.getRequestedPrivateconversations()==false)
+					{
+						PrivateconversationService.setRequestedPrivateconversations();
+					
+				
+					io.socket.get("/privateconversation",{or:[{'talker1':MyID},{'talker2':MyID}],limit:30000},
+		function (pc) {
+				console.log("in blk promise recieved reply");
+				for (x in pc)
+				{
+					
+				
+				PrivateconversationService.setPrivateconversation(pc[x]);	
+				
+				}
+				$rootScope.$broadcast('new pc');
+				});	
+					}
+					)
+					);
+					}
+			}
+              
+        
+        
+        }
+	
+	
+	  $scope.$on('new pc', function(event, args) {
+				
+		
+		$scope.$apply(function(){
+	  $scope.PrivateConversations=PrivateConversationService.getPrivateConversations();
+	});
+	});
+
 
 }]);
