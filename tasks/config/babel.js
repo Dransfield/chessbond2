5ba20bb523d88614d21470d56a7374f3631cc178
slenkar <slenkar@gmail.com>
2017-03-29 07:14:40 -0400

    hi

--- tasks/config/babel.js
+++ tasks/config/babel.js
@@ -2,6 +2,9 @@ module.exports = function(grunt) {
 
     grunt.config.set('babel', {
       dev: {
+		  options: {
+                presets: ['es2015', 'react']
+            },
         files: [{
           expand: true,
           cwd: 'assets/js/',
