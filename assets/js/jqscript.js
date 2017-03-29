06060402daec19feff7f395785dbf942a4fddac1
slenkar <slenkar@gmail.com>
2017-03-29 18:56:30 -0400

    hi

--- assets/js/jqscript.js
+++ assets/js/jqscript.js
@@ -6,6 +6,12 @@ var Accounts{};
 			myuser=usr;
 			console.log(JSON.stringify(myuser));
 			Accounts[usr.id]=usr;
+			showUsername($("#usr",usr);
 			$("#usr").html(Accounts[usr.id].name);
 		});
 //});
+
+function showUsername(elem,usracc)
+{
+elem.html(usracc.FideTitle+" "+usracc.name);	
+}
