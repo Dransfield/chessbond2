d01591a2612973fa7dcca2ee8171e29cbdfaa6da
slenkar <slenkar@gmail.com>
2017-03-29 11:30:23 -0400

    hi

--- assets/js/jqscript.js
+++ assets/js/jqscript.js
@@ -1,10 +1,10 @@
 	var myuser;
 
-$(document).ready(){
+$(document).ready(function(){
  io.socket.get('/user/'+MyID,
 					function(usr){
 			myuser=usr;
 			console.log(JSON.stringify(myuser));
 			$("#usr").html(myuser.name);
 		});
-};
+});
