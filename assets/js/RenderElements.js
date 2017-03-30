a0a805a9b38245643e7d488bec5dc7a7f6b6fcd0
slenkar <slenkar@gmail.com>
2017-03-30 13:04:15 -0400

    hi

--- assets/js/RenderElements.js
+++ assets/js/RenderElements.js
@@ -45,7 +45,7 @@ function showNavbar(elem,usracc)
 				 <span class="badge" id="NumberofNotificationsSpan" ></span>
 				 </li>
 				  <li  class="dropdown">
-					<a    href="#" class="dropdown-toggle navbar-brand" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false" id="NameDiv" ></a>
+					<a    href="#" class="dropdown-toggle navbar-brand" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"  ><span id="NameDiv" class='caret'></span></a>
 					<ul class="dropdown-menu">
 						<li><a href="/profile/<%- req.session.passport.user%>" >My Profile</a></li>
 						<li><a href="/albums/<%- req.session.passport.user%>" >My Albums</a></li>
@@ -79,7 +79,7 @@ function showNavbar(elem,usracc)
 	if(Accounts[usracc])
 	{
 		console.log("Welcome "+Accounts[usracc].name);
-		$("#NameDiv").html("<span class='caret'></span>Welcome "+Accounts[usracc].name);
+		$("#NameDiv").insertAfter("Welcome "+Accounts[usracc].name);
 	if(Accounts[usracc].Invisible)
              {
 				$("#InvisibleMessage").html("Days Left To Account Deletion:"+Accounts[usracc].DaysToDelete);
