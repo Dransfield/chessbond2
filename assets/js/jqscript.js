7af99be6a881505496b7089805307b3b5aa612a0
slenkar <slenkar@gmail.com>
2017-03-29 21:14:32 -0400

    hi

--- assets/js/jqscript.js
+++ assets/js/jqscript.js
@@ -1,5 +1,37 @@
 	var myuser;
 var Accounts={};
+var OpenGames={};
+
+var p1 = new Promise((resolve, reject) => {
+$http.get('/openchessgame?limit=3000').then( function (dat) {
+			
+			resolve(dat.data);
+			//for(x in dat.data)
+			//{
+			//dat.data[x].phrase=phrasefordate(dat.data[x].createdAt);
+			//$scope.opg.push(dat.data[x]); // => {id:9, name: 'Timmy Mendez'}
+			
+			//}
+			
+			
+			});
+		}
+		
+var p2 = new Promise((resolve, reject) => {
+
+		io.socket.get("/chessgame",{or:[{'Player1':MyID},{'Player2':MyID}],limit:30000},
+		function (resData,jwres){
+			resolve(resData);
+}		
+
+Promise.all([p1, p2]).then(values => { 
+  console.log(values); // [3, 1337, "foo"] 
+for (x in values[0])
+{
+console.log(values[0][x].Player1);
+}
+});
+
 //io.socket.on('connect',function(data){
  io.socket.get('/user/'+MyID,
 					function(usr){
