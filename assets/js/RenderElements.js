72ec4f61389b88c1ebc5f3a3909f63e3e816667b
slenkar <slenkar@gmail.com>
2017-04-04 11:31:47 -0400

    hi

--- assets/js/RenderElements.js
+++ assets/js/RenderElements.js
@@ -78,7 +78,7 @@ elem.append("<span  href='/profile/"+usracc+"' id='usernamedropdown"+usracc+"' c
   function handlerOut()
   {console.log("handler out!");}
   
-$("#usernamedropdown"+usracc).hover(handlerIn,handlerOut);
+$("#usernamedropdown"+usracc).hover(function(){console.log("in");},function(){console.log("out");});
  
 }
 
