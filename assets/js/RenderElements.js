9d18797d330700a45007d9317ffe72f46856f589
slenkar <slenkar@gmail.com>
2017-04-04 19:21:17 -0400

    hi

--- assets/js/RenderElements.js
+++ assets/js/RenderElements.js
@@ -107,12 +107,12 @@ elem.append("<div>Deleted Account</div>");
 		$("#usernamedropdown"+usracc+"-"+thisuserprinted).mouseenter(function()
 		{//console.log("MOUSE ENTER");
 			//console.log(DropDowns[usracc]);
-			$("#usernamedropdown"+usracc+"-"+thisuserprinted).append(DropDowns[usracc]);
+			$("#usernamedropdown"+usracc+"-"+thisuserprinted).append(DropDowns[usracc]).hide().slideDown();
 			});
 		$("#usernamedropdown"+usracc+"-"+thisuserprinted).mouseleave(function()
 		{//console.log("MOUSE LEAVE");
 			//console.log(DropDowns[usracc]);
-			DropDowns[usracc].detach();
+			DropDowns[usracc].slideUp().detach();
 			});
 	
 /*
