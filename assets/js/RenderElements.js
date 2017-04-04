07ccccfcd5142170be384e2bb5ce18483af700af
slenkar <slenkar@gmail.com>
2017-04-04 09:39:57 -0400

    hi

--- assets/js/RenderElements.js
+++ assets/js/RenderElements.js
@@ -207,7 +207,7 @@ function showNewGameControls(elem){
 	
 	for (giter in gamecategories)
 	{
-	$("#gamecategories").append("<select>"+gamecategories[x].time+" "+gamecategories[x].extratime+"</select>");
+	$("#addGameCategories").append("<select>"+gamecategories[x].time+" "+gamecategories[x].extratime+"</select>");
 	}
 	
 elem.append(`	 <div class="row">
