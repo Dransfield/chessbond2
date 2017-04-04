f68f0565a635f83184b72c25135327ed27ab1415
slenkar <slenkar@gmail.com>
2017-04-04 18:07:50 -0400

    hi

--- assets/js/RenderElements.js
+++ assets/js/RenderElements.js
@@ -71,6 +71,7 @@ var gamecategories=[{time:1,extratime:0},
 function CreateDropDown(usracc)
 {
 	DropDowns[usracc]= $("<div class='userdropdown'>JQ DROPDOWN</div>");
+	DropDowns[usracc].append("<ul class='userdropdown-content' ><li><a href="#">Cumulative Rating "+Accounts[usracc].ELO+"</a></li>)");
 }
 
 function showUsername(elem,usracc)
