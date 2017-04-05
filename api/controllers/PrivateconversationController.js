c1dedc53d9f973e10e94314686e2daa90980aed4
slenkar <slenkar@gmail.com>
2017-04-04 21:40:42 -0400

    hi

--- api/controllers/PrivateconversationController.js
+++ api/controllers/PrivateconversationController.js
@@ -8,8 +8,8 @@
 module.exports = {
 		New:function(req,res){
 		//console.log(req.param('user')+" is "+req.param('idlestatus'));
-	sails.sockets.broadcast(req.param('Talker1'),'PrivateConversationStarted', {user:req.param('Talker2')});
-	sails.sockets.broadcast(req.param('Talker2'),'PrivateConversationStarted', {user:req.param('Talker1')});
+	sails.sockets.broadcast(req.param('Talker1'),'PrivateConversationStarted', {user:req.param('Talker2'),id:req.param('id')});
+	sails.sockets.broadcast(req.param('Talker2'),'PrivateConversationStarted', {user:req.param('Talker1'),id:req.param('id')});
 	
 	}
 };
