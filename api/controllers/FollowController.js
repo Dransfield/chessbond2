6b3f3c80395368d39644e61d5717ce8b3d5ddb3a
slenkar <slenkar@gmail.com>
2017-04-05 18:58:59 -0400

    hi

--- api/controllers/FollowController.js
+++ api/controllers/FollowController.js
@@ -8,8 +8,8 @@
 module.exports = {
 		New:function(req,res){
 		//console.log(req.param('user')+" is "+req.param('idlestatus'));
-	sails.sockets.broadcast(req.param('Follower'),'FollowStarted', {user:req.param('Followed'),id:req.param('id')});
-	sails.sockets.broadcast(req.param('Followed'),'FollowStarted', {user:req.param('Follower'),id:req.param('id')});
+	sails.sockets.broadcast(req.param('follower'),'FollowStarted', {user:req.param('followed'),id:req.param('id')});
+	sails.sockets.broadcast(req.param('followed'),'FollowStarted', {user:req.param('follower'),id:req.param('id')});
 	
 	}
 };
