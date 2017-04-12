f56a9040846629a9b2f29bf00ae592a63229b073
slenkar <slenkar@gmail.com>
2017-04-12 16:44:59 -0400

    hi

--- assets/js/RenderElements.js
+++ assets/js/RenderElements.js
@@ -414,8 +414,8 @@ function showTextwithInput(elem,words,elemTochange)
 	elem.append(myinput);
 	myinput.change(function(){
 		Accounts[ProfID][words]=myinput.val();
-		elemTochange.val(Accounts[ProfID][words]);
-		elemTochange.inner(Accounts[ProfID][words]);
+		//elemTochange.val(Accounts[ProfID][words]);
+		elemTochange.html(Accounts[ProfID][words]);
 		});
 }
 function showAnchorButton(elem,words,linkto,btnstyle="btn-success"){
