ade86521bde767b292d12defe0639e8e0616d8c3
slenkar <slenkar@gmail.com>
2017-04-13 19:25:57 -0400

    hi

--- assets/js/jqscript.js
+++ assets/js/jqscript.js
@@ -203,7 +203,7 @@ function setupProfilePage()
 			block2.append("<span>Edit:</span>");
 			var sel=showSelect(block2,["Male","Female"],["Male","Female"]);	
 			sel.change(function(e)
-			{console.log(e);
+			{console.log(JSON.stringify(e));
 				myspan.html(e.target.selectedOptions.value);
 				
 				console.log(e.target.selectedOptions.value);
