9c56242de04bc2ecd79243ae8bc5584054798d3e
slenkar <slenkar@gmail.com>
2017-04-16 09:44:23 -0400

    hi

--- assets/js/jqscript.js
+++ assets/js/jqscript.js
@@ -325,7 +325,7 @@ function setupProfilePage()
 			if (texteditable==2)
 				{
 			
-				var block2=addFlexDiv(tbl,two+"city","row","nowrap");
+				var block2=addFlexDiv(tbl,two+"city","row","wrap");
 				
 				if (bckgrd==0)
 				{block2.css("background-color","#e0e0eb");}
