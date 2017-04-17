7daa381b0d73c6ddbdc30d952a0b22508bc8567c
slenkar <slenkar@gmail.com>
2017-04-17 14:42:11 -0400

    hi

--- assets/js/RenderElements.js
+++ assets/js/RenderElements.js
@@ -753,9 +753,9 @@ function showRecentGames(elem,usracc)
 	//console.log("JoinedGames[ProfID][iter][0].id "+JoinedGames[ProfID][iter][0].id);
 	//console.log("JoinedGames[ProfID][iter][0].GameCategory "+JoinedGames[ProfID][iter][0].GameCategory);
 	timeFlex.append("<p>"+JoinedGames[usracc][iter].GameCategory+"</p>");
-{
+	moveFlex.append("<p>"+JoinedGames[usracc][iter].Moves+"</p>");
 	
-}
+
 	//console.log(JoinedGames[ProfID][iter].Player1);
 	}
    /*
