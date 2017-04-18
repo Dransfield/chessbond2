82f429fa8810cbf0f36c60a32f791c0b32c01873
slenkar <slenkar@gmail.com>
2017-04-18 10:56:59 -0400

    hi

--- assets/js/RenderElements.js
+++ assets/js/RenderElements.js
@@ -632,7 +632,7 @@ elem.append(`<nav class="navbar navbar-default navbar-inverse">
 `);
 */
 
-NavbarDropDown=$("<ul style='z-index:99;width:inherit;position:absolute;right:10px;background-color:white;padding:30px 5px 10px 5px;box-shadow: 10px 10px 5px grey;'></ul>");
+NavbarDropDown=$("<ul style='z-index:9999;width:inherit;position:absolute;right:10px;background-color:white;padding:30px 5px 10px 5px;box-shadow: 10px 10px 5px grey;'></ul>");
 NDDlinks={};
 NDDlinks['ProfileLink']=$("<a  id='profilelink' href='/profile/"+Accounts[MyID].id+"'><li style='list-style-position: inside;color:black'> My Profile</li></a>");
 NDDlinks['AlbumLink']=$("<a id='albumlink' href='/albums/"+Accounts[MyID].id+"' ><li style='list-style-position: inside;color:black'>My Albums</li></a>");
