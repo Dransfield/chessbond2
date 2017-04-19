f96db38ca95202ae673ee8a45c2418fabac7ccb2
slenkar <slenkar@gmail.com>
2017-04-19 14:44:16 -0400

    hi

--- assets/js/jqscript.js
+++ assets/js/jqscript.js
@@ -311,7 +311,7 @@ function setupProfilePage()
 			
 			function addvarpair(tbl,one,two,texteditable,bckgrd)
 			{
-			var block=addFlexDiv(tbl,two+"flex","row","nowrap");
+			var block=addFlexDiv(tbl,two+"flex","row","nowrap","space-between");
 			if (bckgrd==0)
 			{block.css("background-color","#e0e0eb");}
 			
