3ff5b43eb9cc8083972cdbc03601ba0501f9fb5f
slenkar <slenkar@gmail.com>
2017-04-18 20:07:34 -0400

    hi

--- assets/js/jqscript.js
+++ assets/js/jqscript.js
@@ -113,7 +113,32 @@ var GamePlaying={};
 				topPlayerMarque.append("timer");
 				addSpan(boardDivDiv,"boardcontainer");
 				 board1 = ChessBoard('boardcontainer',{draggable: true,onDrop: onDrop,onSnapEnd:onSnapEnd,pieceTheme: '/img/chesspieces/'+$scope.User.ChessPieceTheme[0]+'/{piece}.png'} );
- 
+				
+				GamePlaying.PlayerOnBottom='White';
+	if (GamePlaying.Player2==MyID){
+		console.log("im player2");
+		console.log("GamePlaying.Player1Color "+GamePlaying.Player1Color);
+		if (GamePlaying.Player1Color=='White')
+		{
+		board1.flip();
+		
+		GamePlaying.PlayerOnBottom='Black';
+		}
+	
+	}
+		
+	if (GamePlaying.Player1==MyID){
+		if (GamePlaying.Player1Color=='Black')
+		{
+		board1.flip();
+	
+		GamePlaying.PlayerOnBottom='Black';
+		
+		}
+	}
+				
+				
+				
 				});
 			});
 		});
