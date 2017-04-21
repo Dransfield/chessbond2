ef9daef1abaf2e389ea906ea5f398dbe7447715b
slenkar <slenkar@gmail.com>
2017-04-21 09:52:02 -0400

    hi

--- assets/js/jqscript.js
+++ assets/js/jqscript.js
@@ -136,7 +136,7 @@ var soundVolume=5;
 				var horizontalDiv=addFlexDiv(overall,"horizontal","row","wrap","flex-start");
 				var boardDivDiv=addFlexDiv(horizontalDiv,"bdd","column","wrap","flex-start");
 				//showBoardOptions(boardDivDiv);
-				var topPlayerMarque=addFlexDiv(boardDivDiv,"topPlayerMarque","row","nowrap","flex-start","center");
+				var topPlayerMarque=addFlexDiv(boardDivDiv,"topPlayerMarque","row","nowrap","space-around","center");
 				var topMinutes=addSpan(topPlayerMarque,"topminutes");
 				topPlayerMarque.append("<img style='position:relative;' src='/images/eye2.png' height='30px'>");
 				showsmallAvatar(topPlayerMarque,GamePlaying.Player1);
