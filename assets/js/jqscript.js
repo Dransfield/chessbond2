66176df3023b0cee5739e3838f87a0809d646673
slenkar <slenkar@gmail.com>
2017-04-23 12:21:50 -0400

    hi

--- assets/js/jqscript.js
+++ assets/js/jqscript.js
@@ -174,7 +174,7 @@ var soundVolume=5;
 					if (boardSizeValues[sIter].value==Accounts[MyID].BoardSize)
 					{
 						console.log("setting bdd to "+Accounts[MyID].BoardSize);
-					horizontalDiv.css("height",boardSizeValues[sIter].value+"%");
+					boardDivDiv.css("width",boardSizeValues[sIter].value+"%");
 					}
 				}
 				var boardcontainer=addFlexDiv(boardDivDiv,"boardcontainer","row","wrap","flex-start");
