6df45a4ac55f3e101035c28af86bfbd871c0465a
slenkar <slenkar@gmail.com>
2017-04-23 14:31:19 -0400

    hi

--- assets/js/RenderElements.js
+++ assets/js/RenderElements.js
@@ -622,10 +622,10 @@ function showBoardOptions(elem)
 		topPlayerMarque.css("width","100%");
 		bottomPlayerMarque.css("width","100%");
 		$("#sideBoard").css("height",$("#bdd").css("height"));
-		$("#chatDiv").css("height","100%");
+		$("#chatDiv").css("max-height","100%");
 		$("#chatDiv").css("overflow","auto");
-	//	$("#chatDiv2").css("height",$("#sideBoard").css("height"));
-		
+		$("#chatDiv2").css("max-height","100%");
+		$("#chatDiv2").css("overflow","auto");
 		console.log("sideboard new height "+$("#sideBoard").css("height"));
 		console.log("chatdiv1 new height "+$("#chatDiv").css("height"));
 		console.log("chatdiv2 new height "+$("#chatDiv2").css("height"));
