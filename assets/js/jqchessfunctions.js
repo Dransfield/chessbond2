a6d12965a428c4815c93c0cfbc984c0fea2516d6
slenkar <slenkar@gmail.com>
2017-04-27 11:53:36 -0400

    hi

--- assets/js/jqchessfunctions.js
+++ assets/js/jqchessfunctions.js
@@ -294,6 +294,7 @@ function showCapturedPieces()
 		if(blackcap[x])
 		{
 			console.log("showing "+blackcap[x]);
+			blackcap[x]=blackcap[x].toUpper();
 	$("#capturedPieces").append("<img src='/img/chesspieces/"+Accounts[MyID].ChessPieceTheme+"/b"+blackcap[x]+".png'>");	
 	}
 	}
@@ -308,6 +309,7 @@ function showCapturedPieces()
 		console.log(whitecap[x]);
 	if( whitecap[x])
 	{
+			whitecap[x]=whitecap[x].toUpper();
 	$("#capturedPieces").append("<img src='/img/chesspieces/"+Accounts[MyID].ChessPieceTheme+"/w"+whitecap[x]+".png'>");	
 	}
 	}
