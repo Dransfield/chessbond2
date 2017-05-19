e2288bca9292ae96407a691a26d190b3a7c7676e
slenkar <slenkar@gmail.com>
2017-05-19 06:46:07 -0400

    hi

---
+++ api/controllers/RematchController.js
@@ -0,0 +1,55 @@
+/**
+ * RematchController
+ *
+ * @description :: Server-side logic for managing rematches
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+function makeGame(remid)
+
+{
+		Rematch.findOne(id:remid, function foundRematch(err, rem) {
+			Chessgame.create({Player1ELO:OppoELO,Player1CategoryELO:OppoCategoryELO,Player2ELO:MyELO,Player2CategoryELO:MyCategoryELO,GameCategory:ThisGameCat,Player1TimeLimit:num1,Player1TimeLeft:num1,Player2TimeLimit:num2,Player2TimeLeft:num1,GameType:GameTypeID,Move:1,Player1Color:OppoColor,Player1:OppoID,Player2:MyID,Player1Name:OppoName,Player2Name:MyName}).exec(
+			
+			function (err, records) {
+				if(err){
+				}
+				
+				
+				});
+		});
+	
+	
+}
+
+module.exports = {
+	
+	WantRematch: function(req,res)  {
+		Rematch.findOne(game:req.param('gam'), function foundRematch(err, rem) {
+			if(rem)
+			{
+				if(rem.Player1)	
+				{
+					if(rem.Player1!=req.me)
+					{
+					rem.Player2=req.me;
+					makeGame(rem.id);
+					}
+				}
+			}
+			else
+			{
+			
+			Rematch.create({Player1:req.param('me'),game:req.param('gam')}).exec(function (err, records) {
+		
+			sails.sockets.broadcast(req.param('gam'),'rematch', {content:req.param('me')});
+		
+			return res.ok();
+			});
+				
+			}
+		});
+		
+	}
+};
+
