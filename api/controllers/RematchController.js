5e7b684442fdd4ca4c9e16ca7fc5cdb352d2eba6
slenkar <slenkar@gmail.com>
2017-05-22 20:11:09 -0400

    hi

--- api/controllers/RematchController.js
+++ api/controllers/RematchController.js
@@ -7,6 +7,7 @@
 
  function MakeGame(p1,p2,p1color,gamecat,gametype,num1,num2)
  {
+	 console.log("makegame function");
 	User.find({
 	id : [p1,p2]
 	}).exec(function (err, players){
@@ -56,6 +57,7 @@
 		p2ELO=p2Record.ELO;
 		p2CategoryELO=p2Record['rating'+p2color+gamecat];
 		
+		console.log(p1Name+" "+p2Name);
 		
 		
 		Chessgame.create({Player1ELO:p1ELO,Player1CategoryELO:p1CategoryELO,Player2ELO:p2ELO,Player2CategoryELO:p2CategoryELO,GameCategory:gamecat,Player1TimeLimit:num1,Player1TimeLeft:num1,Player2TimeLimit:num2,Player2TimeLeft:num2,GameType:gametype,Move:1,Player1Color:p1color,Player1:p1ID,Player2:p2ID,Player1Name:p1Name,Player2Name:p2Name}).exec(
