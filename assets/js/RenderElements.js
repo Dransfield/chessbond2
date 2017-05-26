09ca1ba9abdd6a74866cb71dc69a2eb3ff65ae45
slenkar <slenkar@gmail.com>
2017-05-26 11:24:22 -0400

    hi

--- assets/js/RenderElements.js
+++ assets/js/RenderElements.js
@@ -1175,11 +1175,11 @@ function showRecentGames(elem,usracc)
 		var span=addFlexDiv(overallFlex,'id',"row","nowrap");
 	var leftbut=showButton(span,"<","KgreenElement KbigButton");
 	
-	showHeader(span,2,"Recent Games");
+	showHeader(span,2,"Recent Games ("+JoinedGames.length+")");
 	var rightbut=showButton(span,">","KgreenElement KbigButton");
 	
 	rightbut.click(function(){
-		if (JoinedGames.length>recentGameIndex+recentGamesToShow)
+		if (JoinedGames.length>(recentGameIndex+recentGamesToShow))
 		{
 		recentGameIndex=recentGameIndex+recentGamesToShow;
 		}
