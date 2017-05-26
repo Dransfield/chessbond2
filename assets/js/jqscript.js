cd4ba146a22719fb97ddbae73ca23815226028a5
slenkar <slenkar@gmail.com>
2017-05-26 10:04:28 -0400

    hi

--- assets/js/jqscript.js
+++ assets/js/jqscript.js
@@ -859,7 +859,7 @@ function setupProfilePage()
 			var games=3;
 			showRecentGames(leftcol,ProfID);
 			leftcol.css("align-items","flex-start");
-			var chatDiv=addFlexDiv(leftcol,"chatdiv","column",'wrap');
+			var chatDiv=addFlexDiv(tblRight,"chatdiv","column",'wrap');
 			chatDiv.css("align-items","flex-start");
 			showChatForm(chatDiv,ProfID,"wall","none");
 			//console.log(JSON.stringify(WallPosts));
