76c5d55a7159a3a5193c7fca86c57f286ed85fa3
slenkar <slenkar@gmail.com>
2017-05-31 21:29:46 -0400

    hi

--- api/controllers/AvatarController.js
+++ api/controllers/AvatarController.js
@@ -9,13 +9,13 @@ module.exports = {
 	deletepic:function(req,res)
 	{
 	fs=require('fs');
-		fs.unlinkSync(req.param('adr'));
+		
 		Avatar.destroy({id:req.param('picid')}).exec
 	(function(err){
 		if (err) {
   console.log(err);
 		}});
-		
+		fs.unlinkSync(req.param('adr'));
 		return res.json({id:req.param('picid')});
 	}
 };
