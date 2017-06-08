50bb1a4a319f9ab80759dd705226d73f2d81f774
slenkar <slenkar@gmail.com>
2017-06-08 16:29:28 -0400

    ho

--- assets/js/RenderElements.js
+++ assets/js/RenderElements.js
@@ -1510,7 +1510,7 @@ if(Notifications.length>0)
 	for (notIter in Notifications)
 	{
 		NDDlinks['NotificationsLink'+notIter]=$("<span><li style='list-style-position: inside;color:black'>"+Notifications[notIter].msg+"</li></span>");
-		NDDlinks['NotificationsLink'+notIter].click({thisadr:Notifications[notIter].adr},visitNotification;
+		NDDlinks['NotificationsLink'+notIter].click({thisadr:Notifications[notIter].adr},visitNotification);
 	}
 }
 console.log("sosedred");
