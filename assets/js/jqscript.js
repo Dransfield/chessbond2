ddc0b82318f9d94da19a6a2a8dc5157ec502e5ab
slenkar <slenkar@gmail.com>
2017-06-12 19:39:37 -0400

    hi

--- assets/js/jqscript.js
+++ assets/js/jqscript.js
@@ -72,7 +72,14 @@ var Notifications=[];
 				console.log(JSON.stringify(data));
 				Notifications.push(data);
 				$("#NumberofNotificationsSpan").html(Notifications.length);
-
+					for (notIter in Notifications)
+				{
+				NDDlinks['NotificationsLink'+notIter]=$("<span id='notification"+notIter+"'><li style='list-style-position: inside;color:black'>"+Notifications[notIter].msg+"</li></span>");
+				NDDlinks['NotificationsLink'+notIter].click({thisadr:Notifications[notIter].adr},visitNotification);
+				$("#notification"+notIter).detach();
+				$("#navbardropdown").append(NDDlinks['NotificationsLink'+notIter]);
+				}
+				
 			});
 		io.socket.on('IdleNotification',function (data)
 			{
