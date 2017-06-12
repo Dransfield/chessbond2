1e2d9d254b0c4a55ef30ed943274dc1c5d289f93
slenkar <slenkar@gmail.com>
2017-06-12 19:21:32 -0400

    hi

--- api/controllers/WallpostController.js
+++ api/controllers/WallpostController.js
@@ -42,7 +42,7 @@ module.exports = {
 									function (err, records) 
 									{
 
-									sails.sockets.broadcast(records.reciever,'notification', records);
+									//sails.sockets.broadcast(records.reciever,'notification', records);
 	
 									
 									}
