10d929146a194fa8715fd4371c8e739fd613db6f
slenkar <slenkar@gmail.com>
2017-06-12 20:32:25 -0400

    hi

--- assets/js/jqscript.js
+++ assets/js/jqscript.js
@@ -77,7 +77,7 @@ var Notifications=[];
 						console.log(Notifications[notIter].msg);
 					//NDDlinks['NotificationsLink'+notIter]=$("<span id='notification"+notIter+"'><li style='list-style-position: inside;color:black'>"+Notifications[notIter].msg+"</li></span>");
 				//	NDDlinks['NotificationsLink'+notIter].click({thisadr:Notifications[notIter].adr},visitNotification);
-					$("#notification"+notIter).detach();
+					$("#notification"+notIter).remove();
 					//console.log(NDDlinks['NotificationsLink'+notIter]);
 					//console.log($("#navbardropdown"));
 					//$("#navbardropdown").append(NDDlinks['NotificationsLink'+notIter]);
