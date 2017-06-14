71b53c844f9363045540f0be773b2a8a9667e27c
slenkar <slenkar@gmail.com>
2017-06-14 19:48:17 -0400

    hi

--- assets/js/RenderElements.js
+++ assets/js/RenderElements.js
@@ -1230,7 +1230,7 @@ function censor(wrds)
 	
 	var moddedWord=BannedWords[iter].word.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
 console.log(moddedWord);
-console.log(new RegExp("/"+moddedWord+"/i"));
+console.log(new RegExp(moddedWord,'i'));
 	wrds=  wrds.replace(new RegExp("/"+moddedWord+"/i"), 'Christmas');
 	}
 return wrds;
