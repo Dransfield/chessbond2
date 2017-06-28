7b6deaf3b52359df90fb259bf2392e2057ff8117
root <slenkar@gmail.com>
2017-06-28 19:34:37 +0800

    hi

--- assets/js/RenderElements.js
+++ assets/js/RenderElements.js
@@ -152,7 +152,7 @@ function timeOfDayForDate(dat)
 			var hour = nu.getUTCHours() + 1; //months from 1-12
 			var minute = nu.getUTCMinutes();
 			var second = nu.getUTCSeconds();
-	return hour+":"+minute+":"+second;
+	return hour+":"+minute+":"+second+" UTC (London Time)";
 }
 
 function phraseforloggedindate(dat)
