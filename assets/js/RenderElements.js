3e749185224b5cd1394c5313c627206911684aa2
root <slenkar@gmail.com>
2017-07-10 20:58:37 +0800

    hi

--- assets/js/RenderElements.js
+++ assets/js/RenderElements.js
@@ -3466,7 +3466,7 @@ function addGamesToRecentGames2(usracc)
 	if (Accounts[MyID].admin)
 	{
 		var delBut=showButton(newFlex,"Delete Game","KregularButton KredElement");
-		delbut.click({gam:iter,acc:usracc},DeleteGame);
+		delBut.click({gam:iter,acc:usracc},DeleteGame);
 	}
 	
 	if(JoinedGames[usracc][iter].Player1Color=='White')
