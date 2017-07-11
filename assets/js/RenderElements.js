09c9831bb436c61de6b99bd9ecb6b9a65667a1fb
root <slenkar@gmail.com>
2017-07-11 10:36:36 +0800

    hi

--- assets/js/RenderElements.js
+++ assets/js/RenderElements.js
@@ -495,8 +495,9 @@ function showStatTable(elem)
 			
 		if(categoryShowString=="10|0" && gameMatchesCategory(d,categoryShowString))
 		{
-		console.log(playerIsWhite(ProfID,d));	
-		console.log (d.Player1!=d.Player2);
+		console.log("is player white?"+playerIsWhite(ProfID,d));	
+		console.log(d);
+		console.log(d.Player1!=d.Player2);
 		console.log(d.Result!="Both Players Timed Out");
 		}
 		var returner=gameMatchesCategory(d,categoryShowString) && playerIsWhite(ProfID,d) && d.Player1!=d.Player2 && d.Result!="Both Players Timed Out";
