c1d11a5804add5758fe15137e1ba9a48240906b2
root <slenkar@gmail.com>
2017-07-20 18:48:07 +0800

    hi

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -35,7 +35,8 @@ module.exports = {
 	},
 	updateField:function(req,res){
 		//{acc:usracc,field:words,datas:Accounts[usracc][words]},
-		User.update({id:req.param('acc')},{req.param('field'):req.param('datas'),ProfileUpdated:Date.now()}).exec(function afterwards(err,updated){
+		User.update({id:req.param('acc')},{req.param('field'):req.param('datas'),ProfileUpdated:(Date.now())})
+		.exec(function afterwards(err,updated){
 			res.json(updated);
 		});
 	},
