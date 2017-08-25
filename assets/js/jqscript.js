a15c0a6ce35d6a4b53c2b5f2c690413eec4421bb
slenkar <slenkar@gmail.com>
2017-08-24 20:00:15 -0400

    hi

--- assets/js/jqscript.js
+++ assets/js/jqscript.js
@@ -1174,7 +1174,7 @@ function renderAlbumPage(iter)
 						
 									//$("#leftbut").val(">("+imgIndex+")");
 									console.log("button"+thisbuttonnumber);
-									console.log($("#button"+thisbuttonnumber).attr("id");
+									console.log($("#button"+thisbuttonnumber).attr("id"));
 									
 									$("#button"+thisbuttonnumber).val(">("+imgIndex+")");
 									$("#button"+thisbuttonnumber).text(">("+imgIndex+")");
