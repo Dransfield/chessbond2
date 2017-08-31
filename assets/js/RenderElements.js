bdc170f118f9d72c00fab25a13a08b9c51d17d83
slenkar <slenkar@gmail.com>
2017-08-31 15:15:03 -0400

    hi

--- assets/js/RenderElements.js
+++ assets/js/RenderElements.js
@@ -3671,6 +3671,7 @@ if(bookmarks.length>0)
 {
 	for (bookIter in bookmarks)
 	{
+		console.log(bookmarks[bookIter].observed);
 		NDDlinks['BookmarksLink'+bookIter]=$("<span id='bookmark"+bookIter+"'><li style='list-style-position: inside; cursor:pointer;color:black'>"+Accounts[bookmarks[bookIter].observed].name+"</li></span>");
 		NDDlinks['BookmarksLink'+bookIter].click({thisadr:"/profile/"+bookmarks[bookIter].observed},visitBookmark);
 	}
