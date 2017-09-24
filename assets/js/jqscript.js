2978f04df808b4c48f40587f5af370b5013727f7
slenkar <slenkar@gmail.com>
2017-09-24 18:47:34 -0400

    hi

--- assets/js/jqscript.js
+++ assets/js/jqscript.js
@@ -1744,7 +1744,8 @@ var cg = new Promise
 		io.socket.get("/chessgame",{or:[{'Player1':persons[x]},{'Player2':persons[x]}],limit:30000},
 		function (resData,jwres){
 			
-			
+			if(jwres.statusCode!=403)
+			{
 			if (resData)
 			{
 			resData.sort(function(b,a)
@@ -1773,6 +1774,7 @@ var cg = new Promise
 			//console.log(JSON.stringify(JoinedGames[persons[x]][1]));
 			//console.log(JSON.stringify(JoinedGames[persons[x]]['0'][1]));
 			resolve(resData);
+			}
 			}
 		});		
 });
