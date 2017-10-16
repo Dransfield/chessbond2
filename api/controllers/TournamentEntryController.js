727b9e74d9dd9fcae3cd6a86ecd43753c8e1c4e0
slenkar <slenkar@gmail.com>
2017-10-16 14:13:45 -0400

    hi

--- api/controllers/TournamentEntryController.js
+++ api/controllers/TournamentEntryController.js
@@ -8,17 +8,17 @@
 module.exports = {
 	join:function(req,res){
 		console.log("tourn entry join");
-	Tournamententry.create({player:req.param['player'],tournid:req.param['tourny']}).
+	Tournamententry.create({player:req.param('player'),tournid:req.param('tourny')}).
 	exec(function afterwards(err, records)
 			{
 				
 		console.log(err);
 		console.log(JSON.stringify(records));		
 				
-				Tournamententry.find({tournid:req.param['tourny']}).
+				Tournamententry.find({tournid:req.param('tourny')}).
 	exec(function afterwards(err,records)
 	{
-	Tournament.update({id:req.param['tourny']},{players:records.length}).
+	Tournament.update({id:req.param('tourny')},{players:records.length}).
 	exec(function afterwards(err,records)
 		{
 		
