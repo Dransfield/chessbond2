464fe6e8c364a232add6de316a5accf25ade220b
slenkar <slenkar@gmail.com>
2017-10-25 14:17:35 -0400

    hi

--- api/controllers/PageController.js
+++ api/controllers/PageController.js
@@ -565,7 +565,7 @@ function CreateTournaments()
 			console.log("index of "+latestOne.category+" is "+JSON.stringify(indexNum));
 			
 			var arr1=gamecategores.slice(indexNum);
-			var arr2[];
+			var arr2=[];
 			
 			if(indexNum>0)
 			{
