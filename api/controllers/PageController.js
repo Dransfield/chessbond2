ed9602815dd5a087f547ffab8d8835728bba91a4
slenkar <slenkar@gmail.com>
2017-10-26 14:27:49 -0400

    hi

--- api/controllers/PageController.js
+++ api/controllers/PageController.js
@@ -603,8 +603,11 @@ function CreateTournaments()
 				{
 					var mins=((60*1000)*myInterval);
 						var minuser=seconds_ago*1000;
+						console.log("mins "+mins);
 						console.log("threeMinutes "+threeMinutes);
+						console.log("minuser "+minuser);
 						minuser=threeMinutes-minuser;
+						console.log("threeminutes-minuser "+minuser);
 						var createDelay=mins+minuser;
 						console.log("myInterval "+myInterval);
 						console.log("createDelay "+createDelay);
