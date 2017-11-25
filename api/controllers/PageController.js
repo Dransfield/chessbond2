c0811d5f42b2aedf988ba17727e9ed2c080deee4
slenkar <slenkar@gmail.com>
2017-11-24 20:09:51 -0500

    hi

--- api/controllers/PageController.js
+++ api/controllers/PageController.js
@@ -1045,6 +1045,56 @@ deleteopengame:function(req,res){
 	
 	},
 	
+	
+	joinTournament:function(req,res){
+	console.log("tourn entry join");
+	
+	
+	CurrentTournamententry.find({player:req.param('player')}).
+	exec(function afterwards(questionErr,questionRecords)
+	{
+	
+	if(questionErr)
+	{
+		console.log("questionErr"+questionErr);
+	}
+	
+	if (!questionRecords || questionRecords.length==0)
+	{
+			CurrentTournamententry.create({player:req.param('player'),tournid:req.param('tourny')}).
+			exec(function afterwards(currentErr, currentRecords)
+			{
+		
+			});
+	
+	Tournamententry.create({player:req.param('player'),tournid:req.param('tourny')}).
+	exec(function afterwards(err, records)
+		{
+				
+		console.log("error "+err);
+		//console.log(JSON.stringify(records));		
+				
+			Tournamententry.find({tournid:req.param('tourny')}).
+			exec(function afterwards(err2,records2)
+			{
+				console.log("records2.length "+records2.length);
+				Tournament.update({id:req.param('tourny')},{players:records2.length}).
+				exec(function afterwards(err3,records3)
+				{
+				console.log("records3 "+JSON.stringify(records3[0]));		
+				sails.sockets.broadcast('im online', 'tournament entries',{tournID:records3[0].id,players:records3[0].players});
+				});
+			});
+				
+		});
+	
+	}
+	else
+	{return res.error("Already in a tournament");}
+	});
+}
+	
+	
 	newopengame:function(req,res){
 	console.log("all params of new open game"+JSON.stringify(req.allParams()));
 	Openchessgame.create(
--- config/routes.js
+++ config/routes.js
@@ -34,7 +34,7 @@ module.exports.routes = {
 	
 	'GET /':{view:'jqhp'},
 	'GET /upcomingTournamentsWithTime':'TournamentController.upcomingTournamentsWithTime',
-	'POST /JoinTournament':'TournamentEntryController.join',
+	'POST /JoinTournament':'PageController.joinTournament',
 	'POST /starttempconversation':'TempConversationController.New',
 	'POST /lefttempconversation':'TempConversationController.Left',
 	'GET /myprofilemg': {view: 'messagesjq'},
