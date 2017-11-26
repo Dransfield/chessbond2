9b90cf96cdc3bf0fa9fbf83a16be1bdd7bb80c51
slenkar <slenkar@gmail.com>
2017-11-26 16:19:41 -0500

    hi

--- api/controllers/TournamentEntryController.js
+++ api/controllers/TournamentEntryController.js
@@ -22,41 +22,55 @@ module.exports = {
 			console.log("questionRecords "+questionRecords);
 			console.log("questionRecords length "+questionRecords.length);
 	
+			Tournament.find({id:questionRecords[0].tournid}).exec
+			(function afterwards(findTournamentErr,findTournamentRecords)
+			{
 	
-			if (!questionRecords || questionRecords.length==0)
-			{
-			
-			CurrentTournamententry.create({player:req.param('player'),tournid:req.param('tourny')}).
-			exec(function afterwards(currentErr, currentRecords)
-			{
-			
-			});
-	
-			Tournamententry.create({player:req.param('player'),tournid:req.param('tourny')}).
-			exec(function afterwards(err, records)
-			{
-				
-			console.log("error "+err);
-			//console.log(JSON.stringify(records));		
-				
-				Tournamententry.find({tournid:req.param('tourny')}).
-				exec(function afterwards(err2,records2)
+				if (!findTournamentRecords || findTournamentRecords.length==0)
 				{
-					console.log("records2.length "+records2.length);
-					Tournament.update({id:req.param('tourny')},{players:records2.length}).
-					exec(function afterwards(err3,records3)
+							
+					CurrentTournamententry.destroy({player:req.param('player')}).
+					exec(function afterwards(questionErr,questionRecords)
 					{
-					console.log("records3 "+JSON.stringify(records3[0]));		
-					sails.sockets.broadcast('im online', 'tournament entries',{tournID:records3[0].id,players:records3[0].players});
-					return res.send("Successfully joined tournament");
+						
+					});		
+					
+			
+					CurrentTournamententry.create({player:req.param('player'),tournid:req.param('tourny')}).
+					exec(function afterwards(currentErr, currentRecords)
+					{
+			
 					});
-				});
-				
-			});
 	
-	}
-	else
-	{return res.send("Already in a tournament");}
+				Tournamententry.create({player:req.param('player'),tournid:req.param('tourny')}).
+				exec(function afterwards(err, records)
+				{
+				
+				console.log("error "+err);
+				//console.log(JSON.stringify(records));		
+				
+					Tournamententry.find({tournid:req.param('tourny')}).
+					exec(function afterwards(err2,records2)
+					{
+						console.log("records2.length "+records2.length);
+						Tournament.update({id:req.param('tourny')},{players:records2.length}).
+						exec(function afterwards(err3,records3)
+						{
+						console.log("records3 "+JSON.stringify(records3[0]));		
+						sails.sockets.broadcast('im online', 'tournament entries',{tournID:records3[0].id,players:records3[0].players});
+						return res.send("Successfully joined tournament");
+						});
+					});
+				
+				});
+	
+		}
+		else
+		{
+			return res.send("Already in a tournament");
+		}
+		
+		});
 	});
 }
 };
