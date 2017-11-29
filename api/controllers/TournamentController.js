1e5d74801d7a3d304e00425f6e0ff2ca712c2882
slenkar <slenkar@gmail.com>
2017-11-29 17:30:28 -0500

    hi

--- api/controllers/TournamentController.js
+++ api/controllers/TournamentController.js
@@ -10,10 +10,13 @@ module.exports = {
 	upcomingTournamentsWithTime:function(req,res){
 	Tournament.find({ activated:false,sort: 'timeToAvailable ASC'}).exec(function(err,latestOne)
 		{
+			Tournament.find({ activated:true,sort: 'createdAt ASC'}).exec(function(err2,latestOne2)
+			{
 			var dat=Date.now();
 			console.log(dat);
-			sender={serverTime:dat,tourneys:latestOne};
+			sender={serverTime:dat,tourneys:latestOne+latestOne2};
 			return res.send(sender);
+			});
 		});
 	}
 };
