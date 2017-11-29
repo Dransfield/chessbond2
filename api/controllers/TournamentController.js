a4501db5bbab8b2ca7e93ba66eae66eebeb09955
slenkar <slenkar@gmail.com>
2017-11-29 17:44:45 -0500

    hi

--- api/controllers/TournamentController.js
+++ api/controllers/TournamentController.js
@@ -10,13 +10,13 @@ module.exports = {
 	upcomingTournamentsWithTime:function(req,res){
 	Tournament.find({ activated:false,sort: 'timeToAvailable ASC'}).exec(function(err,latestOne)
 		{
-			Tournament.find({ activated:true,sort: 'createdAt ASC',limit:1}).exec(function(err2,latestOne2)
+			Tournament.find({ activated:true,sort: 'createdAt DESC',limit:1}).exec(function(err2,latestOne2)
 			{
 			var dat=Date.now();
 			console.log(dat);
 			console.log("latestone"+JSON.stringify(latestOne));
 			console.log("latestone2"+JSON.stringify(latestOne2));
-			latestOne.push(latestOne2);
+			latestOne.push(latestOne2[0]);
 			sender={serverTime:dat,tourneys:latestOne};
 			return res.send(sender);
 			});
