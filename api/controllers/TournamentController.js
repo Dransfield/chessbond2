5b31a33a284aa7067b75049c1879aa9a9951dd26
slenkar <slenkar@gmail.com>
2017-12-21 14:45:08 -0500

    hi

--- api/controllers/TournamentController.js
+++ api/controllers/TournamentController.js
@@ -5,27 +5,7 @@
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
 
-module.exports = {
-	
-	upcomingTournamentsWithTime:function(req,res){
-	Tournament.find({ activated:false,sort: 'timeToAvailable ASC'}).exec(function(err,latestOne)
-		{
-			Tournament.find({activated:true,sort: 'createdAt DESC',limit:1}).exec(function(err2,latestOne2)
-			{
-			var dat=Date.now();
-			//console.log(dat);
-			//console.log("latestone"+JSON.stringify(latestOne));
-			//console.log("latestone2"+JSON.stringify(latestOne2));
-			latestOne.push(latestOne2[0]);
-			sender={serverTime:dat,tourneys:latestOne};
-			setupgames(latestOne2.id);
-				
-			
-			return res.send(sender);
-			});
-		});
-	},
-	function setupgames(thetournid)
+function setupgames(thetournid)
 	{
 		
 		setTimeout(function(){
@@ -51,5 +31,27 @@ module.exports = {
 						
 		},180000);
 	}
+
+module.exports = {
+	
+	upcomingTournamentsWithTime:function(req,res){
+	Tournament.find({ activated:false,sort: 'timeToAvailable ASC'}).exec(function(err,latestOne)
+		{
+			Tournament.find({activated:true,sort: 'createdAt DESC',limit:1}).exec(function(err2,latestOne2)
+			{
+			var dat=Date.now();
+			//console.log(dat);
+			//console.log("latestone"+JSON.stringify(latestOne));
+			//console.log("latestone2"+JSON.stringify(latestOne2));
+			latestOne.push(latestOne2[0]);
+			sender={serverTime:dat,tourneys:latestOne};
+			setupgames(latestOne2.id);
+				
+			
+			return res.send(sender);
+			});
+		});
+	}
+	
 };
 
