a20548ba45ac00462a787f21795301fbacee034d
keith ashford <slenkar@cpe-2607-FCC8-AB4C-B900-8A51-FBFF-FEFC-A905.dyn6.twc.com>
2018-03-20 18:37:53 -0400

    h

--- tasks/pipeline.js
+++ tasks/pipeline.js
@@ -31,9 +31,7 @@ var jsFilesToInject = [
   // Dependencies like jQuery, or Angular are brought in here
   'js/dependencies/**/*.js',
 
-  'js/RenderElements.js',
-  'js/jqscript.js',
-  'js/jqchessfunctions.js',
+  
   //'js/AccountService.js',
   // All of the rest of your client-side js files
   // will be injected here in no particular order.
