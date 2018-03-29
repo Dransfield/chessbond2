92d81def19cdad6e20cb1c3dcdaf50f8f75b87b5
keith ashford <slenkar@cpe-2607-FCC8-AB4C-B900-8A51-FBFF-FEFC-A905.dyn6.twc.com>
2018-03-29 19:16:50 -0400

    hi

--- config/connections.js
+++ config/connections.js
@@ -22,9 +22,9 @@
 module.exports.connections = {
 
   ssl: {
-     ca: require('fs').readFileSync(__dirname + './ssl/my_apps_ssl_gd_bundle.crt'),
-     key: require('fs').readFileSync(__dirname + './ssl/my_apps_ssl.key'),
-     cert: require('fs').readFileSync(__dirname + './ssl/my_apps_ssl.crt')
+     ca: require('fs').readFileSync(__dirname + '/ssl/my_apps_ssl_gd_bundle.crt'),
+     key: require('fs').readFileSync(__dirname + '/ssl/my_apps_ssl.key'),
+     cert: require('fs').readFileSync(__dirname + '/ssl/my_apps_ssl.crt')
    },
   /***************************************************************************
   *                                                                          *
