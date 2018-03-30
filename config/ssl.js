7fe2ab019b5a6d0d6e013a9d4a6d9ade7a6c86e4
keith ashford <slenkar@cpe-2607-FCC8-AB4C-B900-8A51-FBFF-FEFC-A905.dyn6.twc.com>
2018-03-30 11:36:32 -0400

    hi

--- config/ssl.js
+++ config/ssl.js
@@ -21,7 +21,7 @@ module.exports = {
 ssl: {
      //ca: require('fs').readFileSync(require('path').resolve(__dirname,'ssl/bundle.crt')),
       ca: files.join(),
-   
+		console.log(ca);
      key: require('fs').readFileSync(require('path').resolve(__dirname,'ssl/chessbond.com.key')),
     cert: require('fs').readFileSync(require('path').resolve(__dirname,'ssl/www_chessbond_com.crt'))
    }
