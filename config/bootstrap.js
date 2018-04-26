c1631a70fcdcfa4a61f02bc0dbd279b7a79fa93f
keith ashford <slenkar@cpe-2607-FCC8-AB4C-B900-8A51-FBFF-FEFC-A905.dyn6.twc.com>
2018-04-26 14:38:31 -0400

    hi

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -8,46 +8,12 @@
  * For more information on bootstrapping your app, check out:
  * http://sailsjs.org/#!/documentation/reference/sails.config/sails.config.bootstrap.html
  */
-
+var http = require( 'http' );
 module.exports.bootstrap = function(cb) {
 
   // It's very important to trigger this callback method when you are finished
   // with the bootstrap!  (otherwise your server will never lift, since it's waiting on the bootstrap)
 console.log("bootstrap");
- var http = require('http');
-var express = require('express');
-var app = express();
-
-// on the request to root (localhost:3000/)
-app.get('/', function (req, res) {
-    res.send('<b>My</b> first express http server');
-});
-
-// On localhost:3000/welcome
-app.get('/welcome', function (req, res) {
-    res.send('<b>Hello</b> welcome to my http server made with express');
-});
-
-// Change the 404 message modifing the middleware
-app.use(function(req, res, next) {
-    res.status(404).send("Sorry, that route doesn't exist. Have a nice day :)");
-});
-
-// start the server in the port 3000 !
-app.listen(80, function () {
-    console.log('Example app listening on port 3000.');
-});
- /*
-  var express = require('express')
-   var app = express();
-   var server = require('http').createServer(app).server.listen(80);
-
-
-console.log("created server");
-
-app.get('/', function(req, res) {
- res.redirect("https://"+req.host+req.url);
-});
-*/
+ http.createServer( sails.hooks.http.app ).listen( 80 );
   cb();
 };
