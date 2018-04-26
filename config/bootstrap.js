b87f04337240b38d94f4b65aac79814195bc4288
keith ashford <slenkar@cpe-2607-FCC8-AB4C-B900-8A51-FBFF-FEFC-A905.dyn6.twc.com>
2018-04-26 14:31:39 -0400

    hi

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -16,6 +16,7 @@ module.exports.bootstrap = function(cb) {
  var http = require('http');
 
 http.createServer(function(request, response){
+	console.log("HELLO?!?!");
   response.writeHead(200, {'Content-type':'text/plan'});
   response.write('Hello Node JS Server Response');
   response.end( );
