48c07eef178669cd5c93b8753f856c995c7ce342
keith ashford <slenkar@cpe-2607-FCC8-AB4C-B900-8A51-FBFF-FEFC-A905.dyn6.twc.com>
2018-04-26 14:22:31 -0400

    hi

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -13,14 +13,14 @@ module.exports.bootstrap = function(cb) {
 
   // It's very important to trigger this callback method when you are finished
   // with the bootstrap!  (otherwise your server will never lift, since it's waiting on the bootstrap)
-  //var express = require('express')
-   //var app = express();
-   //var server = require('http').createServer(app);
+  var express = require('express')
+   var app = express();
+   var server = require('http').createServer(app);
 
-//server.listen(80);
+server.listen(80);
 
-//app.get('/', function(req, res) {
-  //res.redirect("https://"+req.host+req.url);
-//});
+app.get('/', function(req, res) {
+ res.redirect("https://"+req.host+req.url);
+});
   cb();
 };
