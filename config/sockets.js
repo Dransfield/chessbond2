7bb16e750635f89bc8d875f42f3ae569845c07f0
keith ashford <slenkar@cpe-2607-FCC8-AB4C-B900-8A51-FBFF-FEFC-A905.dyn6.twc.com>
2018-06-04 17:20:05 -0400

    hi

--- config/sockets.js
+++ config/sockets.js
@@ -122,10 +122,13 @@ module.exports.sockets = {
   * disconnects                                                              *
   *                                                                          *
   ***************************************************************************/
-  // afterDisconnect: function(session, socket, cb) {
-  //   // By default: do nothing.
-  //   return cb();
-  // },
+   afterDisconnect: function(session, socket, cb) {
+   Subscription.destroy({socketid:socket}).exec(function(){
+	console.log(JSON.stringify(socket));   
+    return cb();
+	
+	});
+   },
 
   /***************************************************************************
   *                                                                          *
